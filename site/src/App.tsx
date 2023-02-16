69c049aadd82aed281f0266f8fd1a327992b449f
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-02-16 16:36:39 +0300

    Extended User component functionality

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header/component"
 import Main   from "./components/Main/component"
 
 export default function App() {
-    return <div>
+    return <div className="App">
         <Header />
         <Main />
     </div>
--- site/src/components/Header/component.tsx
+++ site/src/components/Header/component.tsx
@@ -1,8 +1,10 @@
 import "./style.css"
 
+import User from "components/User/component"
+
 export default function Header() {
     return <header className="Header">
         <div className="logo">POMS</div>
-        <button className="sign-in">Sign In</button>
+        <User />
     </header>
 }
