517e45999200a184d9b5ae13c94148354a573c0b
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-02-18 12:45:12 +0300

    Added ReadOnly generic type

--- api/src/util/type.ts
+++ api/src/util/type.ts
@@ -7,3 +7,15 @@ export type TypeOfResult = "undefined"
                          | "symbol"
                          | "function"
                          | "object"
+
+export type DeepReadonly<T> =
+    T extends (infer R)[] ? DeepReadonlyArray<R> :
+    T extends Function ? T :
+    T extends object ? DeepReadonlyObject<T> :
+    T;
+
+export interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
+
+export type DeepReadonlyObject<T> = {
+    readonly [P in keyof T]: DeepReadonly<T[P]>;
+};
