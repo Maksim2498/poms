77715b34c62e91f6ff2743d276fcfad216547b83
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-02-27 13:56:40 +0300

    Added maximum charaters constraint

--- site/src/components/SignIn.tsx
+++ site/src/components/SignIn.tsx
@@ -83,13 +83,18 @@ export default function SignIn(props: Props) {
     }
 
     function validateLogin(login: string): string | undefined {
-        if (login.match(/\s/))
-            return "Login must not contain with white space"
-
         const MIN_LENGTH = 4
 
         if (login.length < MIN_LENGTH)
             return `Login must be at least ${MIN_LENGTH} characters long`
+
+        const MAX_LENGTH = 4
+
+        if (login.length > MAX_LENGTH)
+            return `Login must not be longer than ${MAX_LENGTH} characters`
+
+        if (login.match(/\s/))
+            return "Login must not contain with white space"
 
         return undefined
     }
@@ -97,7 +102,14 @@ export default function SignIn(props: Props) {
     function validatePassword(password: string): string | undefined {
         const MIN_LENGTH = 4
 
-        return password.length < MIN_LENGTH ? `Password must be at least ${MIN_LENGTH} characters long`
-                                            : undefined
+        if (password.length < MIN_LENGTH)
+            return `Password must be at least ${MIN_LENGTH} characters long`
+
+        const MAX_LENGTH = 255
+
+        if (password.length > MAX_LENGTH)
+            return `Password must not be longer than ${MAX_LENGTH} characters`
+
+        return undefined
     }
 }
