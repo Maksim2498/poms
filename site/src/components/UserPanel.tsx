03ccb504f98d68a320d760a5051949f561a5bdd7
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-03-02 15:57:15 +0300

    Added UserPanel functionaly and style preview

--- site/src/components/UserPanel.tsx
+++ site/src/components/UserPanel.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Button from "./Button"
 
 import "styles/UserPanel.css"
@@ -7,27 +8,29 @@ export type Props = {
 }
 
 export default function UserPanel(props: Props) {
+    const [header, setHeader] = useState("Profile")
+
     return <div className="UserPanel">
         <ul className="content-selector">
             {contentSelectorItems().map((item, i) => <li key={i}>{item}</li>)}
         </ul>
         <div className="content">
-
+            <div className="header">
+                <h2>{header}</h2>
+            </div>
         </div>
     </div>
 
     function contentSelectorItems(): JSX.Element[] {
         const items: JSX.Element[] = [
-            <Button>Profile</Button>
+            <Button onClick={() => setHeader("Server")}>Server</Button>,
+            <Button onClick={() => setHeader("Profile")}>Profile</Button>,
+            <Button onClick={() => setHeader("Users")}>Users</Button>
         ]
 
-        if (!props.isAdmin)
-            return items
+        if (props.isAdmin)
+            items.push(<Button onClick={() => setHeader("Console")}>Console</Button>)
 
-        return [
-            ...items,
-            <Button>Users</Button>,
-            <Button>Console</Button>
-        ]
+        return items
     }
 }
