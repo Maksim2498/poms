ce8b78e45230406034d502e86bccb9cec4300808
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-03-24 01:31:29 +0300

    Refactoring

--- site/src/modules/Main/Component.tsx
+++ site/src/modules/Main/Component.tsx
@@ -1,24 +1,26 @@
-import User            from "logic/User"
-import ContentSelector from "components/ContentSelector/Component"
-import ContentWindow   from "components/ContentWindow/Component"
-import Welcome         from "components/Welcome/Component"
-import Server          from "components/Server/Component"
-import Users           from "components/Users/Component"
-import Profile         from "components/Profile/Component"
-import Console         from "components/Console/Component"
+import User                         from "logic/User"
+import ContentSelector              from "components/ContentSelector/Component"
+import ContentWindow                from "components/ContentWindow/Component"
+import Welcome                      from "components/Welcome/Component"
+import Server                       from "components/Server/Component"
+import Users                        from "components/Users/Component"
+import Profile                      from "components/Profile/Component"
+import Console                      from "components/Console/Component"
 
-import { useState    } from "react"
-import { Content     } from "components/ContentSelector/Component"
+import { useContext, useState     } from "react"
+import { AllowAnonymAccessContext } from "pages/App/Component"
+import { Content                  } from "components/ContentSelector/Component"
 
 import "./style.css"
 
 export interface Props {
-    isAnonymAccessAllowed?: boolean
-    content?:              Content
-    user?:                 User
+    content?: Content
+    user?:    User
 }
 
 export default function Main(props: Props) {
+    const isAnonymAccessAllowed = useContext(AllowAnonymAccessContext)
+    const { user }              = props
     const contentList           = makeContentList()
     const initContent           = props.content ?? contentList[0]
     const [content, setContent] = useState(initContent)
@@ -29,8 +31,6 @@ export default function Main(props: Props) {
     </main>
 
     function makeContentList() {
-        const { isAnonymAccessAllowed, user } = props
-
         const contentList = [
             { name: "Home", component: Welcome },
         ] as Content[]
--- site/src/pages/App/Component.tsx
+++ site/src/pages/App/Component.tsx
@@ -1,10 +1,14 @@
-import Header         from "modules/Header/Component"
-import Main           from "modules/Main/Component"
-import Footer         from "modules/Footer/Component"
-import Loading        from "ui/Loading/Component"
-import useFetchAccess from "./api/useFetchAccess"
+import Header            from "modules/Header/Component"
+import Main              from "modules/Main/Component"
+import Footer            from "modules/Footer/Component"
+import Loading           from "ui/Loading/Component"
+import useFetchAccess    from "./api/useFetchAccess"
+
+import { createContext } from "react"
 
 import "./style.css"
+
+export const AllowAnonymAccessContext = createContext(false)
 
 export default function App() {
     const [isAnonymAccessAllowed, loading] = useFetchAccess()
@@ -14,9 +18,11 @@ export default function App() {
             <Loading />
         </div>
 
-    return <div className="App">
-        <Header show="not-signed-in" />
-        <Main isAnonymAccessAllowed={isAnonymAccessAllowed}/>
-        <Footer />
-    </div>
+    return <AllowAnonymAccessContext.Provider value={isAnonymAccessAllowed}>
+        <div className="App">
+            <Header show="not-signed-in" />
+            <Main />
+            <Footer />
+        </div>
+    </AllowAnonymAccessContext.Provider>
 }
