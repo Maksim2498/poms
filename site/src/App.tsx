52092f0e74fc46b65b278fe95f0d486711414590
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-03-23 16:02:39 +0300

    Added GitHub button

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,7 +1,11 @@
+import GitHub from "ui/GitHub/component"
+
 import "./App.css"
 
 export default function App() {
     return <div className="App">
-
+        <footer>
+            <GitHub />
+        </footer>
     </div>
 }
---
+++ site/src/ui/GitHub/component.tsx
@@ -0,0 +1,10 @@
+import "./style.css"
+
+export default function GitHub() {
+    return <a className = "Github"
+              href      = "https://github.com/Maksim2498/poms"
+              target    = "_blank"
+              rel       = "noreferrer">
+              GitHub
+           </a>
+}
