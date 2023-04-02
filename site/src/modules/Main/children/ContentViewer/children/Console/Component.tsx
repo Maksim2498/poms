5a2d1d4f8905f5619fb8897be3ced9153dc8e2f7
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-02 19:27:49 +0300

    Refactoring

--- site/src/modules/Main/children/ContentViewer/children/Console/Component.tsx
+++ site/src/modules/Main/children/ContentViewer/children/Console/Component.tsx
@@ -1,5 +1,7 @@
-import Terminal                                                    from "components/Terminal/Component"
 import useAsync                                                    from "hooks/useAsync"
+import useForceRerender                                            from "hooks/useForceRerender"
+import ConsoleSocket                                               from "./ConsoleSocket"
+import Terminal                                                    from "components/Terminal/Component"
 import Loading                                                     from "ui/Loading/Component"
 import ErrorText                                                   from "ui/ErrorText/Component"
 
@@ -25,9 +27,10 @@ export default function Console() {
     const authController                      = useContext(AuthControllerContext)
     const [records,   setRecords, recordsRef] = useContext(ConsoleContext)
     const [available, loading,    error     ] = useAsync(async () => isConsoleAvailable(authController))
-    const socket                              = useRef(undefined as WebSocket | undefined)
+    const forceRerender                       = useForceRerender()
+    const socket                              = useRef(undefined as ConsoleSocket | undefined)
+    const reconnectTimeout                    = useRef(undefined as number        | undefined)
     const tryReconnect                        = useRef(true)
-    const reconnectTimeout                    = useRef(undefined as number    | undefined)
 
     useEffect(() => {
         if (error != null)
@@ -41,7 +44,7 @@ export default function Console() {
         
         return () => {
             tryReconnect.current = false
-            socket.current?.close()
+            socket.current?.disconnect()
 
             if (reconnectTimeout.current != null) {
                 clearTimeout(reconnectTimeout.current)
@@ -66,13 +69,15 @@ export default function Console() {
             <ErrorText>Console is unavailable</ErrorText>
         </div>
 
+    const disabled = socket.current?.state !== "authorized"
+
     return <div className="loaded available Console">
-        <Terminal records={records} onEnter={onEnter} disabled={socket.current == null} />
+        <Terminal records={records} onEnter={onEnter} disabled={disabled} />
     </div>
 
     function onEnter(newRecord: Record) {
-        const willSend =  newRecord.type             === "input"
-                       && socket.current?.readyState === WebSocket.OPEN
+        const willSend =  newRecord.type        === "input"
+                       && socket.current?.state === "authorized"
 
         if (willSend)
             socket.current!.send(newRecord.text)
@@ -84,80 +89,53 @@ export default function Console() {
     }
 
     async function initSocket(): Promise<void> {
-        return await new Promise(resolve => {
+        await new Promise<void>(resolve => {
             pushRecord("info", "Connecting...")
 
-            const host = window.location.host
-            const url  = `wss://${host}/ws/console`
+            const newSocket = new ConsoleSocket()
 
-            try {
-                const newSocket          = new WebSocket(url)
-                let   prevState          = newSocket.readyState
-                let   isConnectionFailed = false
+            newSocket.on("connection-lost", () => {
+                pushRecord("error", "Connection lost")
+                initReconnect()
+            })
 
-                newSocket.onopen = event => {
-                    prevState      = newSocket.readyState
-                    socket.current = newSocket
+            newSocket.on("connection-failed", () => {
+                pushRecord("error", "Connection failed")
+                initReconnect()
+            })
 
-                    pushRecord("success", "Connected")
-                    resolve()
-                }
+            newSocket.on("disconnected", () => {
+                socket.current = undefined
+                forceRerender()
+            })
 
-                newSocket.onerror = () => {
-                    if (handleConnectionFailedError())
-                        return
+            newSocket.on("authorization-failed", () => pushRecord("error", "Authorization failed"))
 
-                    prevState      = newSocket.readyState
-                    socket.current = undefined
+            newSocket.on("authorized", () => {
+                pushRecord("success", "Authorized")
+                socket.current = newSocket
+                forceRerender()
+                resolve()
+            })
 
-                    pushRecord("error", "Connection lost")
-                    initReconnection()
-                }
+            newSocket.on("authorizing", () => pushRecord("info", "Authorizing..."))
 
-                newSocket.onmessage = event => pushRecord("output", event.data)
+            newSocket.on("connected", () => pushRecord("success", "Connected"))
 
-                newSocket.onclose = event => {
-                    if (handleConnectionFailedError())
-                        return
+            newSocket.on("messagae", text => pushRecord("output", text))
 
-                    prevState      = newSocket.readyState
-                    socket.current = undefined
-
-                    pushRecord("info", "Connection closed")
-                }
-
-                function handleConnectionFailedError(): boolean {
-                    if (isConnectionFailed)
-                        return true
-
-                    if (prevState === WebSocket.CONNECTING) {
-                        connectionFailed()
-
-                        isConnectionFailed = true
-
-                        return true
-                    }
-
-                    return false
-                }
-            } catch (error) {
-                console.error(error)
-                connectionFailed()
-            }
         })
 
-        function connectionFailed() {
-            pushRecord("error", "Connection failed")
-            initReconnection()
-        }
-
-        function initReconnection() {
+        function initReconnect() {
             if (!tryReconnect.current)
                 return
 
             pushRecord("info", `Reconnecting in ${RECONNECT_INTERVAL} seconds...`)
 
-            reconnectTimeout.current = window.setTimeout(initSocket, 1000 * RECONNECT_INTERVAL)
+            reconnectTimeout.current = window.setTimeout(() => {
+                reconnectTimeout.current = undefined
+                initSocket()
+            }, 1000 * RECONNECT_INTERVAL)
         }
     }
 
---
+++ site/src/modules/Main/children/ContentViewer/children/Console/ConsoleSocket.ts
@@ -0,0 +1,141 @@
+import EventEmitter from "events"
+import Token        from "logic/Token"
+
+export type State = "connecting"
+                  | "connected"
+                  | "authorizing"
+                  | "authorized"
+                  | "disconnecting"
+                  | "disconnected"
+
+export default class ConsoleSocket extends EventEmitter {
+    static readonly URL = `wss://${window.location.host}/ws/console`
+
+    private socket:     WebSocket
+    private authorized: boolean = false
+    private _state:     State   = "connecting"
+
+    constructor() {
+        super()
+
+        this.socket = new WebSocket(ConsoleSocket.URL)
+
+        this.socket.addEventListener("close", () => {
+            if (this._state === "connecting")
+                this.emit("connection-lost")
+
+            this._state = "disconnected"
+
+            this.emit("disconnected")
+        })
+
+        this.socket.addEventListener("error", () => {
+            this._state = "disconnected"
+            this.emit("connection-lost")
+        })
+
+        this.socket.addEventListener("open", () => {
+            this._state = "connected"
+            this.emit("connected")
+        })
+    }
+
+    on(eventName: "connection-lost",      listener: (              ) => void): this
+    on(eventName: "connection-failed",    listener: (              ) => void): this
+    on(eventName: "disconnecting",        listener: (              ) => void): this
+    on(eventName: "disconnected",         listener: (              ) => void): this
+    on(eventName: "authorization-failed", listener: (              ) => void): this
+    on(eventName: "authorized",           listener: (              ) => void): this
+    on(eventName: "authorizing",          listener: (              ) => void): this
+    on(eventName: "connected",            listener: (              ) => void): this
+    on(eventName: "messagae",             listener: (text: string  ) => void): this
+    on(eventName: string | symbol,        listener: (...args: any[]) => void): this {
+        return super.on(eventName, listener)
+    }
+
+    emit(eventName: "connection-lost"               ): boolean
+    emit(eventName: "connection-failed"             ): boolean
+    emit(eventName: "disconnecting"                 ): boolean
+    emit(eventName: "disconnected"                  ): boolean
+    emit(eventName: "authorization-failed"          ): boolean
+    emit(eventName: "authorized"                    ): boolean
+    emit(eventName: "authorizing"                   ): boolean
+    emit(eventName: "connected"                     ): boolean
+    emit(eventName: "messagae",         text: string): boolean
+    emit(eventName: string | symbol, ...args: any[]):  boolean {
+        return super.emit(eventName, ...args)
+    }
+
+    get state(): State {
+        return this._state
+    }
+
+    async disconnect(): Promise<void> {
+        this.checkState([
+            "connecting",
+            "connected",
+            "authorizing",
+            "authorized"
+        ])
+
+        await new Promise(resolve => {
+            this.emit("disconnecting")
+            this.socket.onclose = resolve
+            this.socket.close()
+        })
+    }
+
+    send(message: string) {
+        this.checkState(["authorized"], "send message")
+        this.socket.send(message)
+    }
+
+    async auth(accessToken: string): Promise<boolean> {
+        Token.checkId(accessToken)
+
+        this.checkState(["connected"], "authorize")
+        this._state = "authorizing"
+
+        this.emit("authorizing")
+
+        try {
+            await new Promise<void>((resolve, reject) => {
+                this.socket.onmessage = event => {
+                    const data       = event.data.trim().toLowerCase()
+                    const authorized = data === "true"
+
+                    if (authorized) {
+                        resolve()
+                        return
+                    }
+
+                    reject()
+                }
+
+                this.socket.send(accessToken)
+            })
+
+            this.socket.onmessage = event => this.emit("messagae", event.data)
+            this._state           = "authorized"
+
+            this.emit("authorized")
+
+            return true
+        } catch {
+            this._state = "connected"
+            this.emit("authorization-failed")
+            return false
+        }
+    }
+
+    private checkState(required: State[], action?: string) {
+        if (required.includes(this.state))
+            return
+
+        const requiredString = required.map(r => `"${r}"`).join(", ")
+        const actionString   = action ?? "perform action"
+        const message        = `Cannot ${actionString} while in "${this.state}" state. One of the following states is required: ${requiredString}`
+
+        throw new Error(message)
+    }
+}
