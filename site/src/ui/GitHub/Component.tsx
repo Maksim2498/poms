7d50fb6fb1cd96c6a227fc5a64ed414b91ba7a99
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-07 21:40:43 +0300

    Moved GitHub UI component styles to css modules

--- site/src/ui/GitHub/Component.tsx
+++ site/src/ui/GitHub/Component.tsx
@@ -1,7 +1,7 @@
-import "./style.css"
+import styles from "./styles.module.css"
 
 export default function GitHub() {
-    return <a className = "Github"
+    return <a className = {styles.Github}
               href      = "https://github.com/Maksim2498/poms"
               target    = "_blank"
               rel       = "noreferrer">
