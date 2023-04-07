d21aa388104f435d48efbfdf2e949ed8c181d2d0
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-07 22:18:13 +0300

    Moved UserButton styles to css modules

--- site/src/modules/Header/children/UserButton/Component.tsx
+++ site/src/modules/Header/children/UserButton/Component.tsx
@@ -1,12 +1,11 @@
 import UserName                               from "ui/UserName/Component"
 import Button                                 from "ui/Button/Component"
 import UserIcon                               from "ui/UserIcon/Component"
+import styles                                 from "./styles.module.css"
 
 import { useContext, useState               } from "react"
 import { AuthControllerContext, UserContext } from "pages/App/Component"
 import { deauth                             } from "logic/api"
-
-import "./style.css"
 
 export interface Props {
     onSignIn?: OnSignIn
@@ -17,40 +16,28 @@ export type OnSignIn = () => void
 export default function UserButton(props: Props) {
     const authController                = useContext(AuthControllerContext)
     const [user,        setUser       ] = useContext(UserContext)
-    const [showSignOut, setShowSignOut] = useState(false)
+    const [showOptions, setShowOptions] = useState(false)
 
-    return <div className="UserButton">
-        {body()}
-    </div>
-
-    function body() {
-        return user != null ? signedIn()
-                            : notSignedIn()
-    }
-
-    function signedIn() {
-        return <div className="signed-in">
-            <UserName user={user!} />
-            <UserIcon user={user!} onClick={() => setShowSignOut(!showSignOut)} />
-            <div className="options" style={{ display: showSignOut ? "block" : "none" }}>
-                <Button type="cancel" onClick={onSignOut}>Sign Out</Button>
-            </div>
-        </div>
-
-        async function onSignOut() {
-            try {
-                await deauth(authController)
-            } catch (error) {
-                console.error(error)
-            } finally {
-                setUser(undefined)
-            }
-        }
-    }
-
-    function notSignedIn() {
-        return <div className="not-signed-in">
+    if (user == null)
+        return <div className={`${styles.notSignedIn} ${styles.UserButton}`}>
             <Button type="submit" onClick={props.onSignIn}>Sign In</Button>
         </div>
+
+    return <div className={`${styles.signedIn} ${styles.UserButton}`}>
+        <UserName user={user!} />
+        <UserIcon user={user!} onClick={() => setShowOptions(!showOptions)} />
+        <div className={styles.options} style={{ display: showOptions ? "block" : "none" }}>
+            <Button type="cancel" onClick={onSignOut}>Sign Out</Button>
+        </div>
+    </div>
+
+    async function onSignOut() {
+        try {
+            await deauth(authController)
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setUser(undefined)
+        }
     }
 }
