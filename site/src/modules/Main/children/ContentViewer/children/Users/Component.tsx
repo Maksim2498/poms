d2b53602ef949e71f13436b8635947587f5ace37
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-07 21:54:41 +0300

    Moved Users styles to css modules

--- site/src/modules/Main/children/ContentViewer/children/Users/Component.tsx
+++ site/src/modules/Main/children/ContentViewer/children/Users/Component.tsx
@@ -3,12 +3,11 @@ import User                      from "logic/User"
 import UserCard                  from "ui/UserCard/Component"
 import Loading                   from "ui/Loading/Component"
 import ErrorText                 from "ui/ErrorText/Component"
+import styles                    from "./styles.module.css"
 
 import { useContext, useEffect } from "react"
 import { AuthControllerContext } from "pages/App/Component"
 import { OnUserCardClick       } from "ui/UserCard/Component"
-
-import "./style.css"
 
 export interface Props {
     onUserClick?: OnUserCardClick
@@ -25,16 +24,16 @@ export default function Users(props: Props) {
     }, [error])
 
     if (loading)
-        return <div className="loading Users">
+        return <div className={`${styles.loading} ${styles.Users}`}>
             <Loading />
         </div>
 
     if (error != null)
-        return <div className="error Users">
+        return <div className={`${styles.error} ${styles.Users}`}>
             <ErrorText>Loading failed</ErrorText>
         </div>
 
-    return <ul className="loaded Users">
+    return <ul className={`${styles.loaded} ${styles.Users}`}>
         {users.map(user =>
             <li key={user.login}>
                 <UserCard user={user} onClick={onUserClick} />
