04a72ac14028b2040c7ae0e0c51c0c3f1985295a
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-15 12:44:54 +0300

    BackButton UI component refactoring

--- site/src/ui/BackButton/Component.tsx
+++ site/src/ui/BackButton/Component.tsx
@@ -1,11 +1,7 @@
-import IconButton            from "ui/IconButton/Component"
-import iconPath              from "./icon.svg"
+import IconButton from "ui/IconButton/Component"
+import iconPath   from "./icon.svg"
 
-import { OnIconButtonClick } from "ui/IconButton/Component"
-
-export interface Props {
-    onClick?: OnIconButtonClick
-}
+import { Props  } from "./types"
 
 export default function BackButton(props: Props) {
     return <IconButton src={iconPath} onClick={props.onClick}>Back</IconButton>
