3db7f458676381d91cc53b5985e304c7eecdb39d
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-16 13:52:58 +0300

    JsonViewer UI component refactoring

--- site/src/ui/JsonViewer/Component.tsx
+++ site/src/ui/JsonViewer/Component.tsx
@@ -1,8 +1,6 @@
-import styles from "./styles.module.css"
+import styles    from "./styles.module.css"
 
-export interface Props {
-    json?: any
-}
+import { Props } from "./types"
 
 export default function JsonViewer(props: Props) {
     const json = props.json ?? {}
