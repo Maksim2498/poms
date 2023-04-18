5e631da4fcd0bac914d2a79fc6dc12275d6d4699
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-19 00:12:09 +0300

    Implemented password reset

--- site/src/modules/ContentViewer/Profile/Component.tsx
+++ site/src/modules/ContentViewer/Profile/Component.tsx
@@ -1,24 +1,29 @@
-import useAsync                  from "hooks/useAsync"
-import User                      from "logic/User"
-import TaggedUserName            from "ui/TaggedUserName/Component"
-import UserIcon                  from "ui/UserIcon/Component"
-import UserOnlineIndicator       from "ui/UserOnlineIndicator/Component"
-import UserNicknames             from "ui/UserNicknames/Component"
-import Loading                   from "ui/Loading/Component"
-import ErrorText                 from "ui/ErrorText/Component"
-import UserRegInfo               from "ui/UserRegInfo/Component"
-import styles                    from "./styles.module.css"
+import useAsync                                               from "hooks/useAsync"
+import User                                                   from "logic/User"
+import Button                                                 from "ui/Button/Component"
+import Modal                                                  from "ui/Modal/Component"
+import TaggedUserName                                         from "ui/TaggedUserName/Component"
+import UserIcon                                               from "ui/UserIcon/Component"
+import UserOnlineIndicator                                    from "ui/UserOnlineIndicator/Component"
+import UserNicknames                                          from "ui/UserNicknames/Component"
+import Loading                                                from "ui/Loading/Component"
+import ErrorText                                              from "ui/ErrorText/Component"
+import UserRegInfo                                            from "ui/UserRegInfo/Component"
+import styles                                                 from "./styles.module.css"
 
-import { useContext, useEffect } from "react"
-import { AuthControllerContext } from "App/AuthControllerContext"
-import { UserContext           } from "App/UserContext"
-import { ProfileProps          } from "./types"
+import { useContext, useEffect, useState                    } from "react"
+import { AuthControllerContext                              } from "App/AuthControllerContext"
+import { UserContext                                        } from "App/UserContext"
+import { AnswerStates, ButtonAnswerState, InputAnswerState  } from "ui/Modal/types"
+import { ProfileProps                                       } from "./types"
 
 export default function Profile(props: ProfileProps) {
-    const authController                = useContext(AuthControllerContext)
-    const [contextUser, setContextUser] = useContext(UserContext)
-    const [user,        loading, error] = useAsync(getUser)
-    const { onTagClick, editMode      } = props
+    const authController                          = useContext(AuthControllerContext)
+    const [contextUser, setContextUser          ] = useContext(UserContext)
+    const [user, loading, error                 ] = useAsync(getUser)
+    const [changingPassword, setChangingPassword] = useState(false)
+    const [resetingPassword, setResetingPassword] = useState(false)
+    const { onTagClick, editMode                } = props
 
     useEffect(() => {
         if (error != null)
@@ -44,14 +49,14 @@ export default function Profile(props: ProfileProps) {
         </div>
 
     return <div className={styles.loaded}>
-        {editMode && <div style={{marginBottom: "1rem", color: "red"}}>Profile editing is not implemented</div>}
-
         <div className={styles.icon}>
             <UserIcon user={user!} />
         </div>
+
         <div className={styles.taggedName}>
             <TaggedUserName user={user!} onTagClick={innerOnTagClick} />
         </div>
+
         <div className={styles.onlineIndicator}>
             <UserOnlineIndicator user={user!} />
         </div>
@@ -60,6 +65,51 @@ export default function Profile(props: ProfileProps) {
             <UserRegInfo   user={user!} onTagClick={innerOnTagClick} />
             <UserNicknames user={user!} />
         </div>
+
+        {canChangePassword() && <Button color="red" onClick={() => setChangingPassword(true)}>Change password</Button>}
+        {canResetPassword()  && <Button color="red" onClick={() => setResetingPassword(true)}>Reset password</Button>}
+
+        {
+            changingPassword && <Modal header="Password Changing">
+                {{
+                    cancel: {
+                        type:    "button",
+                        text:    "Cancel",
+                        onClick: () => setChangingPassword(false)
+                    }
+                }}
+            </Modal>
+        }
+
+        {
+            resetingPassword && <Modal header="Password Reseting">
+                {{
+                    password: {
+                        type:         "input",
+                        inputType:    "password",
+                        placeholder:  "New password",
+                        autoComplete: "new-password",
+                        disable:      disablePasswordResetItem,
+                        validate:     User.validatePassword
+                    },
+
+                    cancel: {
+                        type:         "button",
+                        text:         "Cancel",
+                        disable:      disablePasswordResetItem,
+                        onClick:      () => setResetingPassword(false)
+                    },
+
+                    change: {
+                        type:         "button",
+                        text:         "Change",
+                        color:        "green",
+                        disable:      states => (states.password as InputAnswerState).invalid != null,
+                        onClick:      onPasswordReset
+                    }
+                }}
+            </Modal>
+        }
     </div>
 
     async function getUser(): Promise<User> {
@@ -75,5 +125,33 @@ export default function Profile(props: ProfileProps) {
 
     function innerOnTagClick(login: string) {
         onTagClick?.(login, user!.login)
+    }
+
+    function canChangePassword(): boolean {
+        return editMode === true
+            && User.areLoginsEqual(contextUser?.login, user?.login)
+    }
+
+    function canResetPassword(): boolean {
+        return editMode             === true
+            && contextUser?.isAdmin === true
+            && !User.areLoginsEqual(contextUser.login, user?.login)
+    }
+
+    function disablePasswordResetItem(states: AnswerStates) {
+        return (states.change as ButtonAnswerState).loading
+    }
+
+    async function onPasswordReset(states: AnswerStates) {
+        const login    = user!.login
+        const password = (states.password as InputAnswerState).value
+
+        await User.setPassword({
+            authController,
+            login,
+            password
+        })
+
+        setResetingPassword(false)
     }
 }
