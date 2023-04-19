23f9bf456ad1bef9e57126be5295ed20b641f6d0
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-19 15:34:59 +0300

    Added fullscreen button

--- site/src/App/Footer/Component.tsx
+++ site/src/App/Footer/Component.tsx
@@ -1,8 +1,10 @@
-import GitHub from "ui/GitHub/Component"
-import styles from "./styles.module.css"
+import FullscreenButton from "ui/FullscreenButton/Component"
+import GitHub           from "ui/GitHub/Component"
+import styles           from "./styles.module.css"
 
 export default function Footer() {
     return <footer className={styles.footer}>
+        <FullscreenButton />
         <GitHub />
     </footer>
 }
