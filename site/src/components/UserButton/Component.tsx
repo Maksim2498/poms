96e515233a2ce758227a5bd475500628240f1615
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-20 19:25:48 +0300

    UserButton component refactoring

--- site/src/components/UserButton/Component.tsx
+++ site/src/components/UserButton/Component.tsx
@@ -1,34 +1,54 @@
-import UserName                  from "ui/UserName/Component"
-import Button                    from "ui/Button/Component"
-import UserIcon                  from "ui/UserIcon/Component"
-import styles                    from "./styles.module.css"
+import UserName                                     from "ui/UserName/Component"
+import Button                                       from "ui/Button/Component"
+import UserIcon                                     from "ui/UserIcon/Component"
+import styles                                       from "./styles.module.css"
 
-import { useContext, useState  } from "react"
-import { AuthControllerContext } from "App/AuthControllerContext"
-import { UserContext           } from "App/UserContext"
-import { deauth                } from "logic/api"
-import { UserButtonProps       } from "./types"
+import { useContext, useState, useEffect, useRef  } from "react"
+import { AuthControllerContext                    } from "App/AuthControllerContext"
+import { UserContext                              } from "App/UserContext"
+import { deauth                                   } from "logic/api"
+import { UserButtonProps                          } from "./types"
 
 export default function UserButton(props: UserButtonProps) {
     const authController                = useContext(AuthControllerContext)
     const [user,        setUser       ] = useContext(UserContext)
     const [showOptions, setShowOptions] = useState(false)
+    const signedInRef                   = useRef(null as HTMLDivElement | null)
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside)
+
+        return () => document.removeEventListener("click", handleClickOutside)
+        
+        function handleClickOutside(event: MouseEvent) {
+            console.log(signedInRef.current, event.target)
+            if (!signedInRef.current?.contains(event.target as Node)) {
+                console.log("Close")
+                setShowOptions(false)
+            }
+        }
+    }, [signedInRef])
 
     if (user == null)
         return <div className={styles.notSignedIn}>
             <Button color="green" onClick={props.onSignIn}>Sign In</Button>
         </div>
 
-    return <div className={styles.signedIn}>
-        <div className={styles.name}>
-            <UserName user={user!} />
+    return <div className={styles.signedIn} ref={signedInRef}>
+        <div className={styles.user} onClick={() => setShowOptions(!showOptions)}>
+            <div className={styles.name}>
+                <UserName user={user!} />
+            </div>
+            <div className={styles.icon}>
+                <UserIcon user={user!} />
+            </div>
         </div>
-        <div className={styles.icon}>
-            <UserIcon user={user!} onClick={() => setShowOptions(!showOptions)} />
-        </div>
-        <div className={styles.options} style={{ display: showOptions ? "block" : "none" }}>
-            <Button color="red" onClick={onSignOut}>Sign Out</Button>
-        </div>
+
+        {
+            showOptions && <div className={styles.options}>
+                <Button color="red" onClick={onSignOut}>Sign Out</Button>
+            </div>
+        }
     </div>
 
     async function onSignOut() {
