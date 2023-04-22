3e3bda29c2540934976b465993077d993f88130e
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-22 19:41:46 +0300

    Added ThrottlingManager

---
+++ server/src/util/ThrottlingManager.ts
@@ -0,0 +1,30 @@
+import { Logger } from "winston"
+
+export default class ThrottlingManager {
+    private table: Map<string, Promise<void>> = new Map()
+
+    readonly delay:   number
+    readonly logger?: Logger
+
+    constructor(delay: number, logger?: Logger) {
+        this.delay  = delay
+        this.logger = logger
+    }
+
+    async throttle(id: string) {
+        this.logger?.debug(`Throttling ${id}...`)
+
+        let promise = this.table.get(id)
+
+        if (promise == null) {
+            promise = new Promise(resolve => setTimeout(resolve, this.delay))
+            this.table.set(id, promise)
+        }
+
+        await promise
+    
+        this.logger?.debug(`${id} throttling is done`)
+
+        this.table.delete(id)
+    }
+}
