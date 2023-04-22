63a8b8a4cf6cf4061c4e0d30bc432cc3f38d2553
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-22 18:47:44 +0300

    Now server address is clickable

--- site/src/ui/ServerAddress/Component.tsx
+++ site/src/ui/ServerAddress/Component.tsx
@@ -3,7 +3,15 @@ import styles                 from "./styles.module.css"
 import { ServerAddressProps } from "./types";
 
 export default function ServerAddress(props: ServerAddressProps) {
+    const { address } = props.server
+
+    if (address == null)
+        return null
+
     return <div>
-        Address: <span className={styles.address}>{props.server.address}</span>
+        Address:
+        <span className={styles.address} hover-text="Copy" onClick={() => navigator.clipboard.writeText(address)}>
+            {address}
+        </span>
     </div>
 }
