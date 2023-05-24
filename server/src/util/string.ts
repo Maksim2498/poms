177ef33bc058ed734004802f5d56c83a987c8dde
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-05-24 17:59:28 +0300

    Added collapseWs function

--- server/src/util/string.ts
+++ server/src/util/string.ts
@@ -1,3 +1,8 @@
 export function hasWs(str: string): boolean {
     return str.match(/\s/) != null
 }
+
+export function collapseWs(str: string): string {
+    return str.trim()
+              .replaceAll(/\s+/, " ")
+}
