ed82303f4f893a48bd7362c92d906c4f41ddf6be
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-06-14 15:53:38 +0300

    Formatting

--- server/src/util/error/error.ts
+++ server/src/util/error/error.ts
@@ -1,6 +1,6 @@
 export function getStringCode(error: unknown): string | undefined {
     return typeof error === "object"
-        && error !=  null
+        && error != null
         && "code" in error
         && typeof error.code === "string"
             ? error.code
