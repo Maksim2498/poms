e2095513f46d5398b259287670c6e01b8e3e6430
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-01-22 11:59:19 +0300

    Formatting

--- server/src/test/kotlin/ru/fominmv/poms/server/util/StringExtKtTest.kt
+++ server/src/test/kotlin/ru/fominmv/poms/server/util/StringExtKtTest.kt
@@ -1,7 +1,7 @@
 package ru.fominmv.poms.server.util
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 class StringExtKtTest {
     @Test
