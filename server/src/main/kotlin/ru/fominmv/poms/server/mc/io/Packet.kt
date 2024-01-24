27d357fbeed9b92e33f9b5a6f774f409b8e74ee6
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-01-24 19:55:11 +0300

    Added toDataStream method to Packet class

--- server/src/main/kotlin/ru/fominmv/poms/server/mc/io/Packet.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/mc/io/Packet.kt
@@ -1,9 +1,14 @@
 package ru.fominmv.poms.server.mc.io
 
+import java.io.ByteArrayInputStream
+
 class Packet(
     val id:   Int,
     val data: ByteArray,
 ) {
     val size: Int
         get() = McDataOutput.evalVarIntSize(id) + data.size
+
+    fun toDataStream(): McDataInputStream =
+        McDataInputStream(ByteArrayInputStream(data))
 }
