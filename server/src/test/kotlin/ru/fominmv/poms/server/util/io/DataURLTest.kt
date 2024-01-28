c08281445331d410b8a5f53e04165606ba81e8d3
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-01-28 18:23:45 +0300

    Added DataURLTest class

---
+++ server/src/test/kotlin/ru/fominmv/poms/server/util/io/DataURLTest.kt
@@ -0,0 +1,120 @@
+package ru.fominmv.poms.server.util.io
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+import org.springframework.util.MimeType
+
+import ru.fominmv.poms.server.util.declaration
+
+import java.nio.charset.StandardCharsets
+
+class DataURLTest {
+    private val TESTS = listOf(
+        Pair(
+            "data:,A%20brief%20note",
+            DataURL("A brief note".toByteArray(StandardCharsets.US_ASCII))
+        ),
+        Pair(
+            "data:text/plain;charset=iso-8859-1,%3F%3F%3F",
+            DataURL(
+                "Έχώ".toByteArray(StandardCharsets.ISO_8859_1),
+                MimeType("text", "plain", StandardCharsets.ISO_8859_1),
+            ),
+        ),
+        Pair(
+            "data:",
+            DataURL(),
+        ),
+        Pair(
+            "data:,",
+            DataURL(),
+        ),
+        Pair(
+            "data:text/plain,",
+            DataURL(),
+        ),
+        Pair(
+            "data:;base64,",
+            DataURL(),
+        ),
+        Pair(
+            "data:test",
+            DataURL(mimeType = MimeType("test", "*", StandardCharsets.US_ASCII)),
+        ),
+        Pair(
+            "data:,;test",
+            DataURL(";test".toByteArray(StandardCharsets.US_ASCII)),
+        ),
+        Pair(
+            "data:;,test",
+            DataURL("test".toByteArray(StandardCharsets.US_ASCII)),
+        ),
+        Pair(
+            "data:text/plain,test",
+            DataURL("test".toByteArray(StandardCharsets.US_ASCII)),
+        ),
+        Pair(
+            "data:text/plain;charset=US-ASCII,test",
+            DataURL("test".toByteArray(StandardCharsets.US_ASCII)),
+        ),
+        Pair(
+            "data:;charset=UTF-8,Hello",
+            DataURL(
+                "Hello".encodeToByteArray(),
+                MimeType("text", "plain", StandardCharsets.UTF_8),
+            ),
+        ),
+        Pair(
+            "data:,a,b",
+            DataURL("a,b".toByteArray(StandardCharsets.US_ASCII)),
+        ),
+        Pair(
+            "data:;base64",
+            DataURL(),
+        ),
+        Pair(
+            "data:;base64,",
+            DataURL(),
+        ),
+        Pair(
+            "data:;base64,hello",
+            DataURL(),
+        ),
+        Pair(
+            "data:text/html;base64,VGhpcyBpcyBhIHRlc3QK",
+            DataURL(
+                "This is a test\n".toByteArray(StandardCharsets.US_ASCII),
+                MimeType("text", "html", StandardCharsets.US_ASCII),
+            ),
+        ),
+        Pair(
+            "data:text/plain;charset=thing;base64;test",
+            DataURL(mimeType = MimeType("test", "*", StandardCharsets.US_ASCII)),
+        ),
+    )
+
+    @Test
+    fun decode() {
+        val sep = "-".repeat(32)
+
+        println(sep)
+
+        for ((encodedURL, expectedURL) in TESTS) {
+            println("Testing: ${encodedURL.declaration()} == $expectedURL")
+
+            val actualURL = DataURL.decode(encodedURL)
+
+            assertEquals(expectedURL, actualURL)
+        }
+
+        println(sep)
+    }
+
+    @Test
+    fun `should decode the same that encodes`() {
+        TESTS.forEach {  (_, url) ->
+            assertEquals(url, DataURL.decode(url.encode()))
+        }
+    }
+}
