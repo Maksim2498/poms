796334d2357d7d0cd0c24a14a02951dba910c515
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-02-06 13:01:12 +0300

    Refactoring

--- server/src/test/kotlin/ru/fominmv/poms/server/util/text/stringext/StringExtKtTest.kt
+++ server/src/test/kotlin/ru/fominmv/poms/server/util/text/stringext/StringExtKtTest.kt
@@ -3,7 +3,7 @@ package ru.fominmv.poms.server.util.text.stringext
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-class StringExtKtTest {
+class StringExtTest {
     @Test
     fun declaration() {
         assertEquals("\"\"",                              "".declaration()                   )
@@ -28,72 +28,40 @@ class StringExtKtTest {
         )
 
         tests.forEach { (string, utf8Length) ->
-            assertEquals(string.utf8Length, utf8Length)
+            assertEquals(utf8Length, string.utf8Length)
         }
     }
 
     @Test
-    fun isInet4SocketAddress() {
+    fun isIP4Address() {
         val tests = listOf(
-            Pair("0.0.0.0",                   true ),
-            Pair("  0.  0. 0  .0  ",          true ),
-            Pair("  10.  0. 255  .8  : 655 ", true ),
-            Pair("0.0.0.0:",                  false),
-            Pair("0.0.0.0:abcdef",            false),
-            Pair("255.255.255.255:65535",     true ),
-            Pair("255.255.255.255:65536",     false),
-            Pair("256.255.255.255:65535",     false),
-            Pair("-1.255.255.255",            false),
-            Pair("0.0.0",                     false),
-            Pair("0.0.0:255",                 false),
-            Pair("1.2.5.10:255",              true ),
-            Pair("a.b.c.10:255",              false),
-            Pair("not and address",           false),
-            Pair("hey 10.20.30.40 bye",       false),
-            Pair("",                          false),
+            Triple("0.0.0.0",                   PortMode.OPTIONAL, true ),
+            Triple("0.0.0.0",                   PortMode.REQUIRED, false),
+            Triple("  0.  0. 0  .0  ",          PortMode.OPTIONAL, true ),
+            Triple("  10.  0. 255  .8  : 655 ", PortMode.OPTIONAL, true ),
+            Triple("0.0.0.0:",                  PortMode.OPTIONAL, false),
+            Triple("0.0.0.0:abcdef",            PortMode.OPTIONAL, false),
+            Triple("255.255.255.255:65535",     PortMode.OPTIONAL, true ),
+            Triple("255.255.255.255:65535",     PortMode.NO,       false),
+            Triple("255.255.255.255:65536",     PortMode.OPTIONAL, false),
+            Triple("256.255.255.255:65535",     PortMode.OPTIONAL, false),
+            Triple("-1.255.255.255",            PortMode.OPTIONAL, false),
+            Triple("0.0.0",                     PortMode.OPTIONAL, false),
+            Triple("0.0.0:255",                 PortMode.OPTIONAL, false),
+            Triple("1.2.5.10:255",              PortMode.OPTIONAL, true ),
+            Triple("a.b.c.10:255",              PortMode.OPTIONAL, false),
+            Triple("not and address",           PortMode.OPTIONAL, false),
+            Triple("hey 10.20.30.40 bye",       PortMode.OPTIONAL, false),
+            Triple("",                          PortMode.OPTIONAL, false),
         )
 
         val sep = "*".repeat(64)
 
         println(sep)
 
-        for ((string, isAddress) in tests) {
-            println("Testing: ${string.declaration()}.isInet4SocketAddress == $isAddress")
-            assertEquals(isAddress, string.isInet4SocketAddress)
-        }
-
-        println(sep)
-    }
-
-    @Test
-    fun isInet4Address() {
-        val tests = listOf(
-            Pair("0.0.0.0",                   true ),
-            Pair("  0.  0. 0  .0  ",          true ),
-            Pair("  10.  0. 255  .8  : 655 ", false),
-            Pair("0.0.0.0:",                  false),
-            Pair("0.0.0.0:abcdef",            false),
-            Pair("255.255.255.255:65535",     false),
-            Pair("255.255.255.255:65536",     false),
-            Pair("256.255.255.255:65535",     false),
-            Pair("-1.255.255.255",            false),
-            Pair("0.0.0",                     false),
-            Pair("0.0.0:255",                 false),
-            Pair("1.2.5.10:255",              false),
-            Pair("1.2.5.10",                  true ),
-            Pair("a.b.c.10:255",              false),
-            Pair("not and address",           false),
-            Pair("hey 10.20.30.40 bye",       false),
-            Pair("",                          false),
-        )
-
-        val sep = "*".repeat(64)
-
-        println(sep)
-
-        for ((string, isAddress) in tests) {
-            println("Testing: ${string.declaration()}.isInet4Address == $isAddress")
-            assertEquals(isAddress, string.isInet4Address)
+        for ((string, portMode, isAddress) in tests) {
+            println("Testing: ${string.declaration()}.isInet4SocketAddress(${portMode}) == $isAddress")
+            assertEquals(isAddress, string.isIP4Address(portMode))
         }
 
         println(sep)
