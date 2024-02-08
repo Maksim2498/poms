ffe41514b1eadabd45eefb37351d99f8c0c6b20e
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-02-08 14:49:41 +0300

    Splitted StringExtTest into AddressKtTest, MiscTestKt, and UTFKtTest

--- server/src/test/kotlin/ru/fominmv/poms/server/util/text/stringext/AddressKtTest.kt
+++ server/src/test/kotlin/ru/fominmv/poms/server/util/text/stringext/AddressKtTest.kt
@@ -2,37 +2,10 @@ package ru.fominmv.poms.server.util.text.stringext
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+
 import java.net.InetAddress
 
-class StringExtTest {
-    @Test
-    fun declaration() {
-        assertEquals("\"\"",                              "".declaration()                   )
-        assertEquals("\"Hello, World!\"",                 "Hello, World!".declaration()      )
-        assertEquals("\"abc\\t\\b\\n\\r\\'\\\"\\\\\\$\"", "abc\t\b\n\r\'\"\\\$".declaration())
-    }
-
-    @Test
-    fun escapeSpecial() {
-        assertEquals("",                              "".escapeSpecial()                   )
-        assertEquals("Hello, World!",                 "Hello, World!".escapeSpecial()      )
-        assertEquals("abc\\t\\b\\n\\r\\'\\\"\\\\\\$", "abc\t\b\n\r\'\"\\\$".escapeSpecial())
-    }
-
-    @Test
-    fun utf8Length() {
-        val tests = listOf(
-            Pair("Hello, World!", 13),
-            Pair("こんにちは、世界!", 25),
-            Pair("Привет, Мир!", 21),
-            Pair("😂😎🥸🤓", 16),
-        )
-
-        tests.forEach { (string, utf8Length) ->
-            assertEquals(utf8Length, string.utf8Length)
-        }
-    }
-
+class AddressKtTest {
     @Test
     fun isIP4Address() {
         val tests = listOf(
