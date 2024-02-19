a923bd03d7f16f3c4c78753294090a7a9d37918e
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-02-19 15:18:37 +0300

    Refactoring and new methods stubs

--- server/src/main/kotlin/ru/fominmv/poms/server/util/text/stringext/address.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/util/text/stringext/address.kt
@@ -2,10 +2,53 @@ package ru.fominmv.poms.server.util.text.stringext
 
 import java.net.Inet4Address
 import java.net.InetAddress
+import java.net.InetSocketAddress
 
-private val INET_4_ADDRESS_REGEX = Regex(
-    List(4) { "\\s*(\\d+)\\s*" }.joinToString("\\.")
-)
+fun String.toInetSocketAddress(
+    portMode:    PortMode = PortMode.OPTIONAL,
+    defaultPort: UShort   = 0u,
+    resolve:     Boolean  = true,
+): InetSocketAddress =
+    toInetSocketAddressOrNull(portMode, defaultPort, resolve)
+        ?: throw IllegalArgumentException("Bad socket address")
+
+fun String.toInetSocketAddressOrNull(
+    portMode:    PortMode = PortMode.OPTIONAL,
+    defaultPort: UShort   = 0u,
+    resolve:     Boolean  = true,
+): InetSocketAddress? {
+    TODO()
+}
+
+fun String.toIP4AddressInetSocketAddress(
+    portMode:    PortMode = PortMode.OPTIONAL,
+    defaultPort: UShort   = 0u,
+): InetSocketAddress =
+    toInet4AddressInetSocketAddressOrNull(portMode, defaultPort)
+        ?: throw IllegalArgumentException("Bad IPv4 socket address")
+
+fun String.toInet4AddressInetSocketAddressOrNull(
+    portMode:    PortMode = PortMode.OPTIONAL,
+    defaultPort: UShort   = 0u,
+): InetSocketAddress? {
+    TODO()
+}
+
+fun String.toDomainNameInetSocketAddress(
+    portMode:    PortMode = PortMode.OPTIONAL,
+    defaultPort: UShort   = 0u,
+    resolve:     Boolean  = true,
+): InetSocketAddress =
+    toDomainNameInetSocketAddressOrNull(portMode, defaultPort, resolve)
+        ?: throw IllegalArgumentException("Bad domain name socket address")
+
+fun String.toDomainNameInetSocketAddressOrNull(
+    portMode:    PortMode = PortMode.OPTIONAL,
+    defaultPort: UShort   = 0u,
+    resolve:     Boolean  = true,
+): InetSocketAddress? {
+    TODO()
+}
 
 fun String.toInet4Address(): Inet4Address =
     toInet4AddressOrNull() ?: throw IllegalArgumentException("Bad IPv4 address")
@@ -28,10 +71,6 @@ fun String.toInet4AddressOrNull(): Inet4Address? {
 
 val String.isIP4Address: Boolean
     get() = isIP4Address()
-
-private val IP_4_ADDRESS_REGEX = Regex(
-    "${List(4) { "\\s*(\\d+)\\s*" }.joinToString("\\.")}(?::\\s*(\\d+)\\s*)?"
-)
 
 fun String.isIP4Address(portMode: PortMode = PortMode.OPTIONAL): Boolean {
     val match = IP_4_ADDRESS_REGEX.matchEntire(this) ?: return false
@@ -59,14 +98,6 @@ fun String.isIP4Address(portMode: PortMode = PortMode.OPTIONAL): Boolean {
 val String.isDomainName: Boolean
     get() = isDomainName()
 
-private const val MAX_DOMAIN_NAME_LENGTH       = 253
-private const val MAX_DOMAIN_NAME_LABEL_LENGTH = 63
-private const val DOMAIN_NAME_LABEL_PATTERN    = "([a-zA-Z](?:[-a-zA-Z0-9]*[a-zA-Z0-9])?)"
-
-private val DOMAIN_NAME_REGEX = Regex(
-    "\\s*($DOMAIN_NAME_LABEL_PATTERN(?:\\.$DOMAIN_NAME_LABEL_PATTERN)*)\\s*(?::\\s*(\\d+)\\s*)?"
-)
-
 fun String.isDomainName(portMode: PortMode = PortMode.OPTIONAL): Boolean {
     val match = DOMAIN_NAME_REGEX.matchEntire(this) ?: return false
 
@@ -91,6 +122,22 @@ fun String.isDomainName(portMode: PortMode = PortMode.OPTIONAL): Boolean {
 
     return portMode != PortMode.NO
 }
+
+private val IP_4_ADDRESS_REGEX = Regex(
+    "${List(4) { "\\s*(\\d+)\\s*" }.joinToString("\\.")}(?::\\s*(\\d+)\\s*)?"
+)
+
+private val INET_4_ADDRESS_REGEX = Regex(
+    List(4) { "\\s*(\\d+)\\s*" }.joinToString("\\.")
+)
+
+private const val MAX_DOMAIN_NAME_LENGTH       = 253
+private const val MAX_DOMAIN_NAME_LABEL_LENGTH = 63
+private const val DOMAIN_NAME_LABEL_PATTERN    = "([a-zA-Z](?:[-a-zA-Z0-9]*[a-zA-Z0-9])?)"
+
+private val DOMAIN_NAME_REGEX = Regex(
+    "\\s*($DOMAIN_NAME_LABEL_PATTERN(?:\\.$DOMAIN_NAME_LABEL_PATTERN)*)\\s*(?::\\s*(\\d+)\\s*)?"
+)
 
 enum class PortMode {
     NO,
