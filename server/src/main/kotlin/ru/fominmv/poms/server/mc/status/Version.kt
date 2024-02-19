28a1ad75f339217261730438aaf823e1e42669bd
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-02-19 22:25:50 +0300

    Added default values consts for Version class

--- server/src/main/kotlin/ru/fominmv/poms/server/mc/status/Version.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/mc/status/Version.kt
@@ -1,6 +1,11 @@
 package ru.fominmv.poms.server.mc.status
 
 data class Version(
-    val name:     String = "<unknown>",
-    val protocol: Int    = -1,
-)
+    val name:     String = DEFAULT_NAME,
+    val protocol: Int    = DEFAULT_PROTOCOL,
+) {
+    companion object {
+        const val DEFAULT_NAME     = "<unknown>"
+        const val DEFAULT_PROTOCOL = -1
+    }
+}
