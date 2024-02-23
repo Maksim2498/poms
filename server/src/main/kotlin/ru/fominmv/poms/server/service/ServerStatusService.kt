76ddbf285cd2c5c5c499107184ca8f274d760eb2
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-02-23 21:27:27 +0300

    Implemented server status caching

--- server/src/main/kotlin/ru/fominmv/poms/server/service/ServerStatusService.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/service/ServerStatusService.kt
@@ -7,13 +7,35 @@ import ru.fominmv.poms.server.mc.status.ServerStatus
 import ru.fominmv.poms.server.mc.status.ServerStatusProvider
 import ru.fominmv.poms.server.mc.status.io.NetServerStatusProvider
 
+import kotlin.time.Duration
+import kotlin.time.TimeMark
+import kotlin.time.TimeSource
+
 @Service
 class ServerStatusService : ServerStatusProvider {
     @Value("\${poms.mc.server.address}")
     private lateinit var address: String
 
-    private val provider by lazy { NetServerStatusProvider(address) }
+    @Value("\${poms.mc.server.status.cache-for}")
+    private var cacheFor: Duration? = null
+
+    private val statusProvider by lazy { NetServerStatusProvider(address) }
+
+    private var status:  ServerStatus? = null
+    private var lastGet: TimeMark?     = null
 
     override val serverStatus: ServerStatus
-        get() = provider.serverStatus
+        get() {
+            if (useCached)
+                return status!!
+
+            status  = statusProvider.serverStatus
+            lastGet = TimeSource.Monotonic.markNow()
+
+            return status!!
+        }
+
+    private val useCached: Boolean
+        get() = status != null
+             && (lastGet?.elapsedNow() ?: Duration.ZERO) < cacheFor!!
 }
