77bc888769d981c8f6b2cd91f07c776bf7cac408
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-02-24 20:38:43 +0300

    Refactoring

--- server/src/main/kotlin/ru/fominmv/poms/server/mc/status/ServerStatus.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/mc/status/ServerStatus.kt
@@ -6,19 +6,18 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.TextComponent
 
-import ru.fominmv.poms.server.mc.status.io.JSONServerStatusDeserializer
-import ru.fominmv.poms.server.mc.status.io.JSONServerStatusSerializer
+import ru.fominmv.poms.server.mc.status.io.ServerStatusJSONDeserializer
+import ru.fominmv.poms.server.mc.status.io.ServerStatusJSONSerializer
 
 import java.awt.image.BufferedImage
 
 import kotlin.time.Duration
 
-@JsonSerialize(using = JSONServerStatusSerializer::class)
-@JsonDeserialize(using = JSONServerStatusDeserializer::class)
+@JsonSerialize(using = ServerStatusJSONSerializer::class)
+@JsonDeserialize(using = ServerStatusJSONDeserializer::class)
 data class ServerStatus (
     val version:     Version        = Version(),
-    val players:     List<Player>   = emptyList(),
-    val maxPlayers:  UInt           = 0u,
+    val players:     Players        = Players(),
     val description: TextComponent  = Component.empty(),
     val favicon:     BufferedImage? = null,
     val ping:        Duration       = Duration.ZERO,
