72c1e73fc73708db1b4df5a712bff25741cc2cda
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-02-24 21:18:35 +0300

    Players class is now a data class

--- server/src/main/kotlin/ru/fominmv/poms/server/mc/status/Players.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/mc/status/Players.kt
@@ -3,7 +3,7 @@ package ru.fominmv.poms.server.mc.status
 import com.fasterxml.jackson.annotation.JsonInclude
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-class Players(
+data class Players(
     val online: Int           = 0,
     val max:    Int           = 10,
     val sample: List<Player>? = null,
