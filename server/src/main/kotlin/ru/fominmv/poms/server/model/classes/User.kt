18dc745ddede90934f73bdbe14cb075c9349dc13
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-10-28 20:29:23 +0300

    Added User class

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/model/classes/User.kt
@@ -0,0 +1,61 @@
+package ru.fominmv.poms.server.model.classes
+
+import ru.fominmv.poms.server.model.interfaces.events.*
+import ru.fominmv.poms.server.model.interfaces.mutable.Normalizable
+import ru.fominmv.poms.server.util.string.ext.removeWhiteSpace
+import ru.fominmv.poms.server.util.string.Secret
+import ru.fominmv.poms.server.validation.constraints.*
+
+import jakarta.persistence.*
+
+import java.time.Instant
+import java.util.UUID
+
+@Entity
+class User(
+    // Credentials
+
+    @field:Login
+    @Column(unique = true, nullable = false, length = Login.MAX_LENGTH)
+    var login: String,
+
+    @Secret
+    @field:ShortText
+    @Column(nullable = false, length = ShortText.MAX_LENGTH)
+    var password: String = "",
+
+    // Model object
+
+    id: UUID ,
+    now: Instant = Instant.now(),
+    createdAt: Instant = now,
+    modifiedAt: Instant = now,
+) :
+    AbstractModelObject<UUID>(
+        id = id,
+        now = now,
+        createdAt = createdAt,
+        modifiedAt = modifiedAt,
+    ),
+
+    PrePersistEventListener,
+    PreRemoveEventListener,
+    Normalizable
+{
+    // Events
+
+    @PrePersist
+    override fun onPrePersist() =
+        normalize()
+
+    @PreRemove
+    override fun onPreRemove() {
+
+    }
+
+    // Normalization
+
+    override fun normalize() {
+        login = login.removeWhiteSpace().lowercase()
+    }
+}
