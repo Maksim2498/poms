19298b2ee4922742208d80cb83ccd30c0ea9afd2
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-10-28 17:09:57 +0300

    Added AbstractModelObject class

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/model/classes/AbstractModelObject.kt
@@ -0,0 +1,51 @@
+package ru.fominmv.poms.server.model.classes
+
+import org.hibernate.annotations.ColumnDefault
+import org.hibernate.annotations.UpdateTimestamp
+
+import ru.fominmv.poms.server.model.interfaces.mutable.MutableModelObject
+import ru.fominmv.poms.server.util.string.toObjectString
+
+import jakarta.persistence.Column
+import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.Id as JpaId
+import jakarta.validation.constraints.PastOrPresent
+
+import java.time.Instant
+
+@MappedSuperclass
+abstract class AbstractModelObject<Id>(
+    @JpaId
+    override var id: Id,
+
+    now: Instant = Instant.now(),
+
+    @field:PastOrPresent
+    @Column(nullable = false)
+    @ColumnDefault("(CURRENT_TIMESTAMP)")
+    override var createdAt: Instant = now,
+
+    @UpdateTimestamp
+    @field:PastOrPresent
+    @Column(nullable = false)
+    @ColumnDefault("(CURRENT_TIMESTAMP)")
+    override var modifiedAt: Instant = now,
+) : MutableModelObject<Id> {
+    override fun equals(other: Any?): Boolean {
+        if (this === other)
+            return true
+
+        if (javaClass != other?.javaClass)
+            return false
+
+        other as AbstractModelObject<*>
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int =
+        id.hashCode()
+
+    override fun toString(): String =
+        toObjectString()
+}
