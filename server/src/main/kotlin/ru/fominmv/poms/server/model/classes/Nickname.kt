f1b9bea265aca026729e967431a63ed406002db7
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-10-28 20:29:19 +0300

    Added Nickname class

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/model/classes/Nickname.kt
@@ -0,0 +1,51 @@
+package ru.fominmv.poms.server.model.classes
+
+import ru.fominmv.poms.server.model.interfaces.events.*
+import ru.fominmv.poms.server.model.interfaces.mutable.Normalizable
+import ru.fominmv.poms.server.validation.constraints.Nickname as NicknameConstraint
+import ru.fominmv.poms.server.util.string.ext.removeWhiteSpace
+
+import jakarta.persistence.*
+
+import java.time.Instant
+import java.util.*
+
+@Entity
+class Nickname(
+    @field:NicknameConstraint
+    @Column(unique = true, nullable = false, length = NicknameConstraint.MAX_LENGTH)
+    var nickname: String,
+
+    id: UUID,
+    now: Instant = Instant.now(),
+    createdAt: Instant = now,
+    modifiedAt: Instant = now,
+) :
+    AbstractModelObject<UUID>(
+        id = id,
+        now = now,
+        createdAt = createdAt,
+        modifiedAt = modifiedAt,
+    ),
+
+    PrePersistEventListener,
+    PreRemoveEventListener,
+    Normalizable
+{
+    // Events
+
+    @PrePersist
+    override fun onPrePersist() =
+        normalize()
+
+    @PreRemove
+    override fun onPreRemove() {
+
+    }
+
+    // Normalization
+
+    override fun normalize() {
+        nickname = nickname.removeWhiteSpace()
+    }
+}
