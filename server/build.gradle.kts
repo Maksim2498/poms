23c1d26f8fb8fd084776b9ef6e769fc9ad267344
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-10-29 00:04:36 +0300

    Updated Kotlin version

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
+	kotlin("jvm") version "2.0.21"
+	kotlin("plugin.spring") version "2.0.21"
 
 	id("org.springframework.boot") version "3.3.5"
 	id("io.spring.dependency-management") version "1.1.6"
 
-	kotlin("plugin.jpa") version "1.9.25"
+	kotlin("plugin.jpa") version "2.0.21"
 }
 
 group = "ru.fominmv.poms.server"
