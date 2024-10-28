044c5ffdf9ee82826af033ee02f40e2a12187ffb
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-10-28 20:24:55 +0300

    Added Nickname constraint annotation

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/validation/constraints/Nickname.kt
@@ -0,0 +1,22 @@
+package ru.fominmv.poms.server.validation.constraints
+
+import jakarta.validation.Constraint
+import jakarta.validation.constraints.Pattern
+
+import kotlin.annotation.AnnotationTarget.*
+import kotlin.reflect.KClass
+
+@MustBeDocumented
+@Constraint(validatedBy = [])
+@Pattern(regexp = "[\\w_]{${Nickname.MIN_LENGTH},${Nickname.MAX_LENGTH}}")
+@Target(ANNOTATION_CLASS, CONSTRUCTOR, FIELD, FUNCTION, TYPE_PARAMETER, TYPE, VALUE_PARAMETER)
+annotation class Nickname(
+    val message: String = "Not a valid nickname",
+    val groups: Array<KClass<out Any>> = [],
+    val payload: Array<KClass<out Any>> = [],
+) {
+    companion object {
+        const val MIN_LENGTH = 3
+        const val MAX_LENGTH = 16
+    }
+}
