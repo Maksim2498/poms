1a8b3b5b72f3ff36b80d1462877ca6c5ff0250e6
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-10-28 18:48:56 +0300

    Added Login constraint annotation

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/validation/constraints/Login.kt
@@ -0,0 +1,22 @@
+package ru.fominmv.poms.server.validation.constraints
+
+import jakarta.validation.Constraint
+import jakarta.validation.constraints.Pattern
+
+import kotlin.annotation.AnnotationTarget.*
+import kotlin.reflect.KClass
+
+@ShortText
+@MustBeDocumented
+@Constraint(validatedBy = [])
+@Pattern(regexp = "[a-z_][a-z0-9_]*", flags = [Pattern.Flag.CASE_INSENSITIVE])
+@Target(ANNOTATION_CLASS, CONSTRUCTOR, FIELD, FUNCTION, TYPE_PARAMETER, TYPE, VALUE_PARAMETER)
+annotation class Login(
+    val message: String = "Not a valid login",
+    val groups: Array<KClass<out Any>> = [],
+    val payload: Array<KClass<out Any>> = [],
+) {
+    companion object {
+        const val MAX_LENGTH = ShortText.MAX_LENGTH
+    }
+}
