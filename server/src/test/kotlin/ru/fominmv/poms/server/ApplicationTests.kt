c0160682ee75ca349590a3d12e6560117b72dd87
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-10-29 01:48:31 +0300

    Refactoring

--- server/src/test/kotlin/ru/fominmv/poms/server/ApplicationTests.kt
+++ server/src/test/kotlin/ru/fominmv/poms/server/ApplicationTests.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class PomsServerApplicationTests {
+class ApplicationTests {
 	@Test
 	fun contextLoads() {}
 }
