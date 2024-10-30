41e9bd1a5373d6f06f66d42cc9c8a01def821da6
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-10-31 01:47:58 +0300

    Updated WithCredentials interface

--- server/src/main/kotlin/ru/fominmv/poms/server/model/interfaces/immutable/WithCredentials.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/model/interfaces/immutable/WithCredentials.kt
@@ -1,6 +1,14 @@
 package ru.fominmv.poms.server.model.interfaces.immutable
 
+import ru.fominmv.poms.libs.commons.string.ext.removeWhiteSpace
+
 interface WithCredentials {
     val login: String
     val password: String
+
+    fun credentialsEquals(other: WithCredentials): Boolean =
+        loginEquals(other.login) && password == other.password
+
+    fun loginEquals(login: String): Boolean =
+        login.removeWhiteSpace().equals(login.removeWhiteSpace(), ignoreCase = true)
 }
