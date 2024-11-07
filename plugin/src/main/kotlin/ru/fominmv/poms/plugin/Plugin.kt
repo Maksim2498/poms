9dc23e4643a05497b7408058262600c3692b9d99
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-11-07 19:28:01 +0300

    Fixed Plugin class

--- plugin/src/main/kotlin/ru/fominmv/poms/plugin/Plugin.kt
+++ plugin/src/main/kotlin/ru/fominmv/poms/plugin/Plugin.kt
@@ -1,7 +1,13 @@
+package ru.fominmv.poms.plugin
+
 import org.bukkit.plugin.java.JavaPlugin
 
 class Plugin : JavaPlugin() {
     override fun onEnable() {
-        
+
+    }
+
+    override fun onDisable() {
+
     }
 }
