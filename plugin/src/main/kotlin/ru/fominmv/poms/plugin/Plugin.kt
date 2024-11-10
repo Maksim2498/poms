c01bb412154a966ed50506a27fba07ce63595138
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-11-10 18:15:54 +0300

    Added /invite and /user command stubs

--- plugin/src/main/kotlin/ru/fominmv/poms/plugin/Plugin.kt
+++ plugin/src/main/kotlin/ru/fominmv/poms/plugin/Plugin.kt
@@ -2,12 +2,66 @@ package ru.fominmv.poms.plugin
 
 import org.bukkit.plugin.java.JavaPlugin
 
+import ru.fominmv.poms.libs.mc.commands.DispatchingCommandProcessor
+
 class Plugin : JavaPlugin() {
     override fun onEnable() {
-
+        registerCommands()
     }
 
     override fun onDisable() {
 
+    }
+
+    private fun registerCommands() {
+        registerInviteCommand()
+        registerUserCommand()
+    }
+
+    private fun registerInviteCommand() {
+        DispatchingCommandProcessor.ParsingBuilder()
+            .add("delete all") {
+                it.sender.sendMessage("Deleting all invites...")
+                true
+            }
+
+            .add("delete {s}") {
+                it.sender.sendMessage("Deleted invite ${it.processedArgs[1]}")
+                true
+            }
+
+            .add("list") {
+                it.sender.sendMessage("No invites")
+                true
+            }
+
+            .add("{s}") {
+                it.sender.sendMessage("Created invite for ${it.processedArgs[0]}")
+                true
+            }
+
+            .build()
+            .register(this, "invite")
+    }
+
+    private fun registerUserCommand() {
+        DispatchingCommandProcessor.ParsingBuilder()
+            .add("delete all") {
+                it.sender.sendMessage("Deleting all users...")
+                true
+            }
+
+            .add("delete {s}") {
+                it.sender.sendMessage("Deleted user ${it.processedArgs[1]}")
+                true
+            }
+
+            .add("list") {
+                it.sender.sendMessage("No users")
+                true
+            }
+
+            .build()
+            .register(this, "user")
     }
 }
