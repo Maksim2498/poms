6ea821492e711b2b48883d745b0e3bfa3aa79d7a
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-11-11 17:59:32 +0300

    Added ReferencedSyncListDelegate class

---
+++ libs/commons/src/main/kotlin/ru/fominmv/poms/libs/commons/collections/delegates/ReferencedSyncListDelegate.kt
@@ -0,0 +1,65 @@
+package ru.fominmv.poms.libs.commons.collections.delegates
+
+import ru.fominmv.poms.libs.commons.collections.classes.ReferencedSyncList
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class ReferencedSyncListDelegate<
+    in HolderType,
+    EffectiveHolderType,
+
+    ElementType,
+    ListType: MutableList<ElementType>,
+>(
+    private val getListFromHolder: (holder: EffectiveHolderType) -> MutableList<ElementType>,
+    private val updateElementHolder: (element: ElementType, newHolder: EffectiveHolderType) -> Unit,
+    private val convertList: (collection: MutableList<ElementType>) -> ListType,
+    private val getEffectiveHolder: (holder: HolderType) -> EffectiveHolderType,
+) : ReadWriteProperty<HolderType, ListType> {
+    private var proxy: ListType? = null
+
+    override fun getValue(thisRef: HolderType, property: KProperty<*>): ListType =
+        getInitializedProxy(thisRef)
+
+    override fun setValue(thisRef: HolderType, property: KProperty<*>, value: ListType) {
+        if (value === proxy)
+            return
+
+        val initializedProxy = getInitializedProxy(thisRef)
+
+        initializedProxy.clear()
+        initializedProxy.addAll(value)
+    }
+
+    private fun getInitializedProxy(holder: HolderType): ListType {
+        if (proxy == null)
+            proxy = convertList(
+                ReferencedSyncList.doubleProxy(
+                    holder = getEffectiveHolder(holder),
+                    getListFromHolder = getListFromHolder,
+                    updateElementHolder = updateElementHolder,
+                )
+            )
+
+        return proxy ?: throw IllegalStateException("proxy is null")
+    }
+
+    class Reference<
+        in HolderType,
+        EffectiveHolderType,
+
+        ReferenceType,
+    >(
+        private val get: () -> ReferenceType,
+        private val set: (value: ReferenceType) -> Unit,
+        private val getList: (reference: ReferenceType) -> MutableList<EffectiveHolderType>,
+        private val getEffectiveHolder: (holder: HolderType) -> EffectiveHolderType,
+    ) : ReadWriteProperty<HolderType, ReferenceType>
+        by ReferencedSyncCollectionDelegate.Reference(
+            get = get,
+            set = set,
+            getCollection = getList,
+            getEffectiveHolder = getEffectiveHolder,
+        )
+}
