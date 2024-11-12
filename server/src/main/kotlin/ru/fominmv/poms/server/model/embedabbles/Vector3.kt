595cec2309ea01b5d5b6067f00ea7e8670e47651
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-11-12 09:30:32 +0300

    Updated Vector3 class

--- server/src/main/kotlin/ru/fominmv/poms/server/model/embedabbles/Vector3.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/model/embedabbles/Vector3.kt
@@ -2,15 +2,55 @@ package ru.fominmv.poms.server.model.embedabbles
 
 import jakarta.persistence.*
 
+import kotlin.math.sqrt
+
 @Embeddable
 @MappedSuperclass
-class Vector3(
+data class Vector3(
     @Column(nullable = false)
-    var x: Float = 0f,
+    var x: Double = 0.0,
 
     @Column(nullable = false)
-    var y: Float = 0f,
+    var y: Double = 0.0,
 
     @Column(nullable = false)
-    var z: Float = 0f,
-)
+    var z: Double = 0.0,
+) {
+    fun zero(): Vector3 =
+        apply { 0.0 }
+
+    fun negate(): Vector3 =
+        apply { xyz -> -xyz }
+
+    fun normalize(): Vector3 =
+        length.let { length ->
+            apply { xyz ->
+                xyz / length
+            }
+        }
+
+    fun normalized(): Vector3 =
+        length.let { length ->
+            map { xyz ->
+                xyz / length
+            }
+        }
+
+    fun map(map: (component: Double) -> Double): Vector3 =
+        Vector3(
+            x = map(x),
+            y = map(y),
+            z = map(z),
+        )
+
+    fun apply(apply: (component: Double) -> Double): Vector3 {
+        x = apply(x)
+        y = apply(y)
+        z = apply(z)
+
+        return this
+    }
+
+    val length: Double
+        get() = sqrt(x*x + y*y + z*z)
+}
