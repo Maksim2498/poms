df3ec3bbdf98d71bef19e5387588f5f480b3b386
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-11-19 18:52:18 +0300

    Added MutableExpirable interface

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/model/interfaces/mutable/MutableExpirable.kt
@@ -0,0 +1,9 @@
+package ru.fominmv.poms.server.model.interfaces.mutable
+
+import ru.fominmv.poms.server.model.interfaces.immutable.Expirable
+
+import java.time.Instant
+
+interface MutableExpirable : MutableTrackable, Expirable {
+    override var expiresAt: Instant
+}
