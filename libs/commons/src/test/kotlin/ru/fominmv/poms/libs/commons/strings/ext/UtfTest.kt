3af882e3d72a58d2b0a3be4d410c9a9c0282e6d3
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-11-22 19:59:57 +0300

    Added test for utf8Length extension method of the String class

---
+++ libs/commons/src/test/kotlin/ru/fominmv/poms/libs/commons/strings/ext/UtfTest.kt
@@ -0,0 +1,28 @@
+package ru.fominmv.poms.libs.commons.strings.ext
+
+import ru.fominmv.poms.libs.commons.io.printlnSep
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class UtfTest {
+    @Test
+    fun utf8Length() {
+        val tests = listOf(
+            Pair("Hello, World!", 13),
+            Pair("こんにちは、世界!", 25),
+            Pair("Привет, Мир!", 21),
+            Pair("😂😎🥸🤓", 16),
+        )
+
+        printlnSep()
+
+        for ((string, utf8Length) in tests) {
+            print("Testing ${string.declaration()}.ut8Length == $utf8Length")
+            assertEquals(utf8Length, string.utf8Length)
+            println(" ✅")
+        }
+
+        printlnSep()
+    }
+}
