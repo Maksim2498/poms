88eaab4a7f80ae61c7ff60e4054732aa3ac42be7
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-11-25 17:24:59 +0300

    Removed things added in last 2 commits

--- libs/commons/src/main/kotlin/ru/fominmv/poms/libs/commons/collections/ext/conversions.kt
+++
@@ -1,9 +0,0 @@
-package ru.fominmv.poms.libs.commons.collections.ext
-
-import ru.fominmv.poms.libs.commons.collections.interfaces.immutable.ByteArrayListView
-
-fun List<Byte>.toByteArrayOrGetView(): ByteArray =
-    if (this is ByteArrayListView)
-        array
-    else
-        toByteArray()
--- libs/commons/src/main/kotlin/ru/fominmv/poms/libs/commons/collections/interfaces/immutable/ByteArrayListView.kt
+++
@@ -1,5 +0,0 @@
-package ru.fominmv.poms.libs.commons.collections.interfaces.immutable
-
-interface ByteArrayListView : List<Byte> {
-    val array: ByteArray
-}
