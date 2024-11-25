fd0e825b481b8d9f145af92c6f6d87267afb116f
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-11-25 11:55:55 +0300

    Added isHttpQuotedStringToken extension property to the String class

--- libs/commons/src/main/kotlin/ru/fominmv/poms/libs/commons/text/strings/ext/http.kt
+++ libs/commons/src/main/kotlin/ru/fominmv/poms/libs/commons/text/strings/ext/http.kt
@@ -1,6 +1,9 @@
 package ru.fominmv.poms.libs.commons.text.strings.ext
 
-import ru.fominmv.poms.libs.commons.text.chars.ext.isHttpTokenCodePoint
+import ru.fominmv.poms.libs.commons.text.chars.ext.*
 
 val String.isHttpToken: Boolean
     get() = all(Char::isHttpTokenCodePoint)
+
+val String.isHttpQuotedStringToken: Boolean
+    get() = all(Char::isHttpQuotedStringTokenCodePoint)
