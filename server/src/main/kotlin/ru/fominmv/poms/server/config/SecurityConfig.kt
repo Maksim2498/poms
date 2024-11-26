9f6165299592f8fb2e7e146f9f48c2fd1711abd7
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-11-26 15:50:35 +0300

    Updated SecurityConfig class

--- server/src/main/kotlin/ru/fominmv/poms/server/config/SecurityConfig.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/config/SecurityConfig.kt
@@ -1,8 +1,13 @@
+@file:Suppress("DEPRECATION")
+
 package ru.fominmv.poms.server.config
 
 import org.springframework.context.annotation.*
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableMethodSecurity
@@ -14,4 +19,14 @@ class SecurityConfig {
             web.ignoring()
                 .requestMatchers("/h2-console/**")
         }
+
+    @Bean
+    @Profile("!${ApplicationProfile.DEVELOPMENT_DISPLAY_NAME}")
+    fun nonDevelopmentPasswordEncoder(): PasswordEncoder =
+        PasswordEncoderFactories.createDelegatingPasswordEncoder()
+
+    @Bean
+    @Profile(ApplicationProfile.DEVELOPMENT_DISPLAY_NAME)
+    fun developmentPasswordEncoder(): PasswordEncoder =
+        NoOpPasswordEncoder.getInstance()
 }
