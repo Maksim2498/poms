743bf062e6cf46938e94d6f9128c941858828a64
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-03 19:57:07 +0300

    Added UserRepository interface

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/repositories/entities/UserRepository.kt
@@ -0,0 +1,14 @@
+package ru.fominmv.poms.server.repositories.entities
+
+import org.springframework.stereotype.Repository
+
+import ru.fominmv.poms.server.model.entities.User
+import ru.fominmv.poms.server.repositories.base.*
+
+import java.util.*
+
+@Repository
+interface UserRepository:
+    EntityManagementRepository<User>,
+    EntityRepository<User, UUID>,
+    CredentialsRepository<User>
