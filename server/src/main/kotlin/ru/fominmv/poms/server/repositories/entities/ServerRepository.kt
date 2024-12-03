b2e68abf61e5f343c819d98cf06cb9b49b8437ca
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-03 19:57:01 +0300

    Added ServerRepository interface

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/repositories/entities/ServerRepository.kt
@@ -0,0 +1,15 @@
+package ru.fominmv.poms.server.repositories.entities
+
+import org.springframework.stereotype.Repository
+
+import ru.fominmv.poms.server.model.entities.Server
+import ru.fominmv.poms.server.repositories.base.*
+
+import java.util.UUID
+
+@Repository
+interface ServerRepository :
+    EntityManagementRepository<Server>,
+    EntityRepository<Server, UUID>,
+    CredentialsRepository<Server>
+
