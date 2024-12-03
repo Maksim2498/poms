5b5b795e081e8fe0634d7b681570c9bae8e1ec4f
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-03 19:56:35 +0300

    Added ResultDeleteRepository interface

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/repositories/base/ResultDeleteRepository.kt
@@ -0,0 +1,17 @@
+package ru.fominmv.poms.server.repositories.base
+
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.NoRepositoryBean
+import org.springframework.data.repository.Repository
+
+@NoRepositoryBean
+interface ResultDeleteRepository<T, Id> : Repository<T, Id> {
+    @Modifying
+    @Query("DELETE FROM User")
+    fun deleteAllAndCount(): Int
+
+    @Modifying
+    @Query("DELETE FROM User WHERE id = ?1")
+    fun deleteByIdAndCount(id: Id): Int
+}
