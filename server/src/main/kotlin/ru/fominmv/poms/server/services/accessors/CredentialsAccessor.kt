597bf985a95f31bbe66541e4d7df2abb2325b8b1
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-03 20:00:40 +0300

    Added CredentialsAccessor interface

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/services/accessors/CredentialsAccessor.kt
@@ -0,0 +1,50 @@
+package ru.fominmv.poms.server.services.accessors
+
+import ru.fominmv.poms.server.errors.not_found.NotFoundByLoginException
+
+interface CredentialsAccessor<T> {
+    // Exists
+
+    fun allExistsByLogin(logins: Iterable<String>): Boolean =
+        logins.all(::existsByLogin)
+
+    fun existsByLogin(login: String): Boolean =
+        getByLoginOrNull(login) != null
+
+    // Get
+
+    fun getAllByLogin(logins: Iterable<String>): List<T> =
+        logins.map(::getByLogin)
+
+    fun getByLogin(login: String): T =
+        getByLoginOrNull(login) ?: onNotFoundByLogin(login)
+
+    // Get or null
+
+    fun tryGetAllByLogin(logins: Iterable<String>): List<T> =
+        logins.mapNotNull(::getByLoginOrNull)
+
+    fun getByLoginOrNull(login: String): T?
+
+    // Delete
+
+    fun deleteAllByLogin(logins: Iterable<String>) =
+        logins.forEach(::deleteByLogin)
+
+    fun deleteByLogin(login: String) {
+        if (!tryDeleteByLogin(login))
+            onNotFoundByLogin(login)
+    }
+
+    // Try delete
+
+    fun tryDeleteAllByLogin(logins: Iterable<String>): Long =
+        logins.count(::tryDeleteByLogin).toLong()
+
+    fun tryDeleteByLogin(login: String): Boolean
+
+    // Errors
+
+    fun onNotFoundByLogin(login: String): Nothing =
+        throw NotFoundByLoginException(login)
+}
