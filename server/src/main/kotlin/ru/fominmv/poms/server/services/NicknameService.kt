8073d6a2cf9ff01dc7a6260822a41cab39e16981
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-04 17:20:33 +0300

    Updated NicknameService class

--- server/src/main/kotlin/ru/fominmv/poms/server/services/NicknameService.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/services/NicknameService.kt
@@ -7,9 +7,12 @@ import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 import ru.fominmv.poms.libs.commons.numbers.ext.toBoolean
+import ru.fominmv.poms.server.errors.duplicate.NicknameDuplicateException
+import ru.fominmv.poms.server.errors.limit.NicknameLimitException
+import ru.fominmv.poms.server.errors.not_found.NotFoundByNicknameException
 import ru.fominmv.poms.server.model.entities.*
 import ru.fominmv.poms.server.repositories.entities.NicknameRepository
-import ru.fominmv.poms.server.services.accessors.bulk.BulkAccessor
+import ru.fominmv.poms.server.services.accessors.bulk.*
 import ru.fominmv.poms.server.services.accessors.*
 
 import java.util.UUID
@@ -19,15 +22,19 @@ import kotlin.jvm.optionals.getOrNull
 @Service
 class NicknameService(private val nicknameRepository: NicknameRepository) :
     BulkAccessor<Nickname>,
+    BulkOwnerAccessor<Nickname>,
 
     Accessor<Nickname>,
     IdAccessor<Nickname, UUID>,
-    NicknameAccessor<Nickname>,
+    NicknameAccessor<Nickname, String>,
     SaveAccessor<Nickname>
 {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     // Exists
+
+    override fun existsByOwner(owner: User?): Boolean =
+        nicknameRepository.existsByInternalOwner(owner)
 
     override fun exists(value: Nickname): Boolean =
         existsById(value.id)
@@ -43,12 +50,24 @@ class NicknameService(private val nicknameRepository: NicknameRepository) :
     override fun count(): Long =
         nicknameRepository.count()
 
+    override fun countOwner(owner: User?): Long =
+        nicknameRepository.countByInternalOwner(owner)
+
     // Get
 
     // - Many
 
     override fun getAll(pageable: Pageable): Page<Nickname> =
         nicknameRepository.findAll(pageable)
+
+    override fun getAll(): List<Nickname> =
+        nicknameRepository.findAll()
+
+    override fun getAllByOwner(owner: User?, pageable: Pageable): Page<Nickname> =
+        nicknameRepository.findAllByInternalOwner(owner, pageable)
+
+    override fun getAllByOwner(owner: User?): List<Nickname> =
+        nicknameRepository.findAllByInternalOwner(owner)
 
     // - One
 
@@ -68,6 +87,9 @@ class NicknameService(private val nicknameRepository: NicknameRepository) :
                 logger.warn("Deleted all {} nicknames", deleted)
         }
 
+    override fun deleteAllByOwner(owner: User?): Long =
+        nicknameRepository.deleteAllByInternalOwner(owner)
+
     // - One
 
     override fun tryDelete(value: Nickname): Boolean =
@@ -86,13 +108,17 @@ class NicknameService(private val nicknameRepository: NicknameRepository) :
         owner: User,
         id: UUID? = null,
         save: Boolean = false,
-    ): Nickname =
-        create(
+    ): Nickname {
+        if (owner.maxNicknames >= owner.nicknames.size)
+            throw NicknameLimitException()
+
+        return create(
             nickname = nickname,
             owner = owner,
             id = id,
             save = save,
         )
+    }
 
     fun create(
         nickname: String,
@@ -147,4 +173,12 @@ class NicknameService(private val nicknameRepository: NicknameRepository) :
         nicknameRepository.persist(value).apply {
             logger.debug("Saved {}", this)
         }
+
+    // Errors
+
+    override fun onNicknameDuplicate(nickname: String): Nothing =
+        throw NicknameDuplicateException(nickname)
+
+    override fun onNotFoundByNickname(nickname: String): Nothing =
+        throw NotFoundByNicknameException(nickname)
 }
