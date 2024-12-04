a4aeabe1a837c67e4b7833278caa5c80b6e2c716
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-04 13:23:44 +0300

    Added NicknameAccessor interface

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/services/accessors/NicknameAccessor.kt
@@ -0,0 +1,61 @@
+package ru.fominmv.poms.server.services.accessors
+
+import ru.fominmv.poms.server.errors.duplicate.NicknameDuplicateException
+import ru.fominmv.poms.server.errors.not_found.NotFoundByNicknameException
+
+interface NicknameAccessor<T> {
+    // Check
+
+    fun checkIfNicknameIsNew(nickname: String) {
+        if (existsByNickname(nickname))
+            onNicknameDuplicate(nickname)
+    }
+    
+    // Exists
+
+    fun allExistsByNickname(nicknames: Iterable<String>): Boolean =
+        nicknames.all(::existsByNickname)
+
+    fun existsByNickname(nickname: String): Boolean =
+        getByNicknameOrNull(nickname) != null
+
+    // Get
+
+    fun getAllByNickname(nicknames: Iterable<String>): List<T> =
+        nicknames.map(::getByNickname)
+
+    fun getByNickname(nickname: String): T =
+        getByNicknameOrNull(nickname) ?: onNotFoundByNickname(nickname)
+
+    // Get or null
+
+    fun tryGetAllByNickname(nicknames: Iterable<String>): List<T> =
+        nicknames.mapNotNull(::getByNicknameOrNull)
+
+    fun getByNicknameOrNull(nickname: String): T?
+
+    // Delete
+
+    fun deleteAllByNickname(nicknames: Iterable<String>) =
+        nicknames.forEach(::deleteByNickname)
+
+    fun deleteByNickname(nickname: String) {
+        if (!tryDeleteByNickname(nickname))
+            onNotFoundByNickname(nickname)
+    }
+
+    // Try delete
+
+    fun tryDeleteAllByNickname(nicknames: Iterable<String>): Long =
+        nicknames.count(::tryDeleteByNickname).toLong()
+
+    fun tryDeleteByNickname(nickname: String): Boolean
+
+    // Errors
+
+    fun onNicknameDuplicate(nickname: String): Nothing =
+        throw NicknameDuplicateException(nickname)
+
+    fun onNotFoundByNickname(nickname: String): Nothing =
+        throw NotFoundByNicknameException(nickname)
+}
