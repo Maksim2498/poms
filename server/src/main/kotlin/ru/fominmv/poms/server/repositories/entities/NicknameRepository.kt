d493433113c03e18f4dd3ae6caaa5d5770616315
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-04 17:09:01 +0300

    Added owner-related methods to the NicknameRepository interface

--- server/src/main/kotlin/ru/fominmv/poms/server/repositories/entities/NicknameRepository.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/repositories/entities/NicknameRepository.kt
@@ -1,8 +1,10 @@
 package ru.fominmv.poms.server.repositories.entities
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
 
-import ru.fominmv.poms.server.model.entities.Nickname
+import ru.fominmv.poms.server.model.entities.*
 import ru.fominmv.poms.server.repositories.base.*
 
 import java.util.*
@@ -12,9 +14,23 @@ interface NicknameRepository :
     EntityManagementRepository<Nickname>,
     EntityRepository<Nickname, UUID>
 {
+    // Nickname
+
     fun findByNickname(nickname: String): Optional<Nickname>
 
     fun existsByNickname(nickname: String): Boolean
 
     fun deleteByNickname(nickname: String): Long
+
+    // Owner
+
+    fun findAllByInternalOwner(owner: User?, pageable: Pageable): Page<Nickname>
+
+    fun findAllByInternalOwner(owner: User?): List<Nickname>
+
+    fun countByInternalOwner(owner: User?): Long
+
+    fun existsByInternalOwner(owner: User?): Boolean
+
+    fun deleteAllByInternalOwner(owner: User?): Long
 }
