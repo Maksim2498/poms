f929169bd7ab207d434505434580276eb0c74f84
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-04 17:09:08 +0300

    Added BulkOwnerAccessor interface

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/services/accessors/bulk/BulkOwnerAccessor.kt
@@ -0,0 +1,18 @@
+package ru.fominmv.poms.server.services.accessors.bulk
+
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+import ru.fominmv.poms.server.model.entities.User
+
+interface BulkOwnerAccessor<T> {
+    fun existsByOwner(owner: User?): Boolean =
+        countOwner(owner) > 0
+
+    fun countOwner(owner: User?): Long =
+        getAllByOwner(owner).size.toLong()
+
+    fun getAllByOwner(owner: User?, pageable: Pageable = Pageable.unpaged()): Page<T>
+
+    fun deleteAllByOwner(owner: User?): Long
+}
