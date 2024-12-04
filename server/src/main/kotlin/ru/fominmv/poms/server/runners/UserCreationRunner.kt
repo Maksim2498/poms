1aadcb8810bed6d288c30ac80c880cd81590daa7
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-04 22:49:40 +0300

    Added UserCreationRunner class

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/runners/UserCreationRunner.kt
@@ -0,0 +1,59 @@
+package ru.fominmv.poms.server.runners
+
+import org.slf4j.LoggerFactory
+
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Service
+
+import ru.fominmv.poms.server.configs.ApplicationConfig
+import ru.fominmv.poms.server.services.UserService
+import ru.fominmv.poms.server.services.NicknameService
+
+import jakarta.transaction.Transactional
+
+@Service
+@Transactional
+class UserCreationRunner(
+    private val applicationConfig: ApplicationConfig,
+    private val userService: UserService,
+    private val nicknameService: NicknameService,
+) : ApplicationRunner {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    override fun run(args: ApplicationArguments) {
+        forUsers@
+        for (user in applicationConfig.init.users) {
+            logger.debug("Trying to create user {}...", user)
+
+            if (user.id?.let(userService::existsById) == true) {
+                logger.debug("User with id {} already exists. Skipped", user.id)
+                continue
+            }
+
+            if (userService.existsByLogin(user.login)) {
+                logger.debug("User with login {} already exists. Skipped", user.login)
+                continue
+            }
+
+            for (nickname in user.nicknames)
+                if (nicknameService.existsByNickname(nickname)) {
+                    logger.debug("Nickname {} is already occupied. Skipped", nickname)
+                    continue@forUsers
+                }
+
+            userService.create(
+                id = user.id,
+
+                login = user.login,
+                password = user.password,
+
+                nicknames = user.nicknames,
+                maxNicknames = user.maxNicknames,
+
+                rights = user.rights,
+                isBlocked = user.isBlocked,
+            )
+        }
+    }
+}
