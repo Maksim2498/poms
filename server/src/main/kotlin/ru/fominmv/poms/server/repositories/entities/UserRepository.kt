0bf4735b75f40795600f0ee4fc111939424d5140
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-04 21:45:33 +0300

    Added nickname-related methods to the UserRepository interface

--- server/src/main/kotlin/ru/fominmv/poms/server/repositories/entities/UserRepository.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/repositories/entities/UserRepository.kt
@@ -2,6 +2,8 @@ package ru.fominmv.poms.server.repositories.entities
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 import ru.fominmv.poms.server.model.entities.User
@@ -15,6 +17,10 @@ interface UserRepository:
     EntityRepository<User, UUID>,
     LoginRepository<User>
 {
+    companion object {
+        private const val NICKNAME_QUERY_BODY = "FROM User u INNER JOIN u.internalNicknames n WHERE n.nickname = ?1"
+    }
+
     // Creator
 
     fun findAllByInternalCreator(creator: User?, pageable: Pageable): Page<User>
@@ -26,4 +32,16 @@ interface UserRepository:
     fun existsByInternalCreator(creator: User?): Boolean
 
     fun deleteAllByInternalCreator(creator: User?): Long
+
+    // Nicknames
+
+    @Query("SELECT u $NICKNAME_QUERY_BODY")
+    fun findByNickname(login: String): Optional<User>
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END $NICKNAME_QUERY_BODY")
+    fun existsByNickname(login: String): Boolean
+
+    @Modifying
+    @Query("DELETE $NICKNAME_QUERY_BODY")
+    fun deleteByNickname(login: String): Int
 }
