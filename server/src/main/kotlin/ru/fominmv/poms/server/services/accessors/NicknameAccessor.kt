e8810070020ec779ab6ccc040431821467dcd55b
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-04 17:20:26 +0300

    Updated NicknameAccessor interface

--- server/src/main/kotlin/ru/fominmv/poms/server/services/accessors/NicknameAccessor.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/services/accessors/NicknameAccessor.kt
@@ -1,64 +1,59 @@
 package ru.fominmv.poms.server.services.accessors
 
-import ru.fominmv.poms.server.errors.duplicate.NicknameDuplicateException
-import ru.fominmv.poms.server.errors.not_found.NotFoundByNicknameException
-
-interface NicknameAccessor<T> {
+interface NicknameAccessor<T, Nickname> {
     // Check
 
-    fun checkIfAllNicknamesIsNew(nicknames: Iterable<String>) =
+    fun checkIfAllNicknamesIsNew(nicknames: Iterable<Nickname>) =
         nicknames.forEach(::checkIfNicknameIsNew)
 
-    fun checkIfNicknameIsNew(nickname: String) {
+    fun checkIfNicknameIsNew(nickname: Nickname) {
         if (existsByNickname(nickname))
             onNicknameDuplicate(nickname)
     }
     
     // Exists
 
-    fun allExistsByNickname(nicknames: Iterable<String>): Boolean =
+    fun allExistsByNickname(nicknames: Iterable<Nickname>): Boolean =
         nicknames.all(::existsByNickname)
 
-    fun existsByNickname(nickname: String): Boolean =
+    fun existsByNickname(nickname: Nickname): Boolean =
         getByNicknameOrNull(nickname) != null
 
     // Get
 
-    fun getAllByNickname(nicknames: Iterable<String>): List<T> =
+    fun getAllByNickname(nicknames: Iterable<Nickname>): List<T> =
         nicknames.map(::getByNickname)
 
-    fun getByNickname(nickname: String): T =
+    fun getByNickname(nickname: Nickname): T =
         getByNicknameOrNull(nickname) ?: onNotFoundByNickname(nickname)
 
     // Get or null
 
-    fun tryGetAllByNickname(nicknames: Iterable<String>): List<T> =
+    fun tryGetAllByNickname(nicknames: Iterable<Nickname>): List<T> =
         nicknames.mapNotNull(::getByNicknameOrNull)
 
-    fun getByNicknameOrNull(nickname: String): T?
+    fun getByNicknameOrNull(nickname: Nickname): T?
 
     // Delete
 
-    fun deleteAllByNickname(nicknames: Iterable<String>) =
+    fun deleteAllByNickname(nicknames: Iterable<Nickname>) =
         nicknames.forEach(::deleteByNickname)
 
-    fun deleteByNickname(nickname: String) {
+    fun deleteByNickname(nickname: Nickname) {
         if (!tryDeleteByNickname(nickname))
             onNotFoundByNickname(nickname)
     }
 
     // Try delete
 
-    fun tryDeleteAllByNickname(nicknames: Iterable<String>): Long =
+    fun tryDeleteAllByNickname(nicknames: Iterable<Nickname>): Long =
         nicknames.count(::tryDeleteByNickname).toLong()
 
-    fun tryDeleteByNickname(nickname: String): Boolean
+    fun tryDeleteByNickname(nickname: Nickname): Boolean
 
     // Errors
 
-    fun onNicknameDuplicate(nickname: String): Nothing =
-        throw NicknameDuplicateException(nickname)
+    fun onNicknameDuplicate(nickname: Nickname): Nothing
 
-    fun onNotFoundByNickname(nickname: String): Nothing =
-        throw NotFoundByNicknameException(nickname)
+    fun onNotFoundByNickname(nickname: Nickname): Nothing
 }
