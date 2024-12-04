696ca1c1a96cf6e7fedc278391dee086c5450277
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-04 17:28:20 +0300

    Added creator-related methods to the UserRepository interface

--- server/src/main/kotlin/ru/fominmv/poms/server/repositories/entities/UserRepository.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/repositories/entities/UserRepository.kt
@@ -1,6 +1,9 @@
 package ru.fominmv.poms.server.repositories.entities
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
+import ru.fominmv.poms.server.model.entities.Nickname
 
 import ru.fominmv.poms.server.model.entities.User
 import ru.fominmv.poms.server.repositories.base.*
@@ -12,3 +15,16 @@ interface UserRepository:
     EntityManagementRepository<User>,
     EntityRepository<User, UUID>,
     LoginRepository<User>
+{
+    // Creator
+
+    fun findAllByInternalCreator(creator: User?, pageable: Pageable): Page<Nickname>
+
+    fun findAllByInternalCreator(creator: User?): List<Nickname>
+
+    fun countByInternalCreator(creator: User?): Long
+
+    fun existsByInternalCreator(creator: User?): Boolean
+
+    fun deleteAllByInternalCreator(creator: User?): Long
+}
