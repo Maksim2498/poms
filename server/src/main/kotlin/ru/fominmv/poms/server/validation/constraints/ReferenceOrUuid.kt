b3b9e13fc312fc13c10bdf5db9438c0fab3e756c
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-05 14:32:40 +0300

    Added ReferenceOrUuid constraint annotation

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/validation/constraints/ReferenceOrUuid.kt
@@ -0,0 +1,25 @@
+package ru.fominmv.poms.server.validation.constraints
+
+import jakarta.validation.Constraint
+import jakarta.validation.constraints.Pattern
+
+import ru.fominmv.poms.libs.commons.uuids.UUID_PATTERN
+
+import kotlin.annotation.AnnotationTarget.*
+import kotlin.reflect.KClass
+
+@ShortText
+@MustBeDocumented
+@Constraint(validatedBy = [])
+@Pattern(regexp = ReferenceOrUuid.PATTERN)
+@Target(ANNOTATION_CLASS, CONSTRUCTOR, FIELD, FUNCTION, TYPE_PARAMETER, TYPE, VALUE_PARAMETER)
+annotation class ReferenceOrUuid(
+    val message: String = "Not a valid reference or UUID",
+    val groups: Array<KClass<out Any>> = [],
+    val payload: Array<KClass<out Any>> = [],
+) {
+    companion object {
+        const val MAX_LENGTH = ShortText.MAX_LENGTH
+        const val PATTERN = "${Reference.PATTERN}|$UUID_PATTERN"
+    }
+}
