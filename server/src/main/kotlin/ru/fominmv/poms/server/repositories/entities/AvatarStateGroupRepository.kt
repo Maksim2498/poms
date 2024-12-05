f31f77ede59066df20d64258dc82bebd2a59bff1
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-05 15:10:42 +0300

    Added AvatarStateGroupRepository interface

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/repositories/entities/AvatarStateGroupRepository.kt
@@ -0,0 +1,15 @@
+package ru.fominmv.poms.server.repositories.entities
+
+import org.springframework.stereotype.Repository
+
+import ru.fominmv.poms.server.model.entities.AvatarStateGroup
+import ru.fominmv.poms.server.repositories.base.*
+
+import java.util.UUID
+
+@Repository
+interface AvatarStateGroupRepository :
+    EntityManagementRepository<AvatarStateGroup>,
+    EntityRepository<AvatarStateGroup, UUID>,
+    ReferenceRepository<AvatarStateGroup>
+
