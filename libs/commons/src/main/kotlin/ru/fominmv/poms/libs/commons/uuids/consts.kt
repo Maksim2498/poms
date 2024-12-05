6f1ba034435e59d23d42308a10b88ed3a3496209
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-05 14:30:14 +0300

    Added UUID_PATTERN constant

---
+++ libs/commons/src/main/kotlin/ru/fominmv/poms/libs/commons/uuids/consts.kt
@@ -0,0 +1,12 @@
+package ru.fominmv.poms.libs.commons.uuids
+
+const val UUID_PATTERN =
+    "[0-9a-fA-F]{1,8}" +
+    "-" +
+    "[0-9a-fA-F]{1,4}" +
+    "-" +
+    "[0-9a-fA-F]{1,4}" +
+    "-" +
+    "[0-9a-fA-F]{1,4}" +
+    "-" +
+    "[0-9a-fA-F]{1,12}"
