de1d149a824540fd6da3f57408877d60234870da
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-08 13:56:01 +0300

    Removed PathAware and PathAwareWebSocketHandler interfaces

--- server/src/main/kotlin/ru/fominmv/poms/server/api/PathAware.kt
+++
@@ -1,5 +0,0 @@
-package ru.fominmv.poms.server.api
-
-interface PathAware {
-    val path: String
-}
--- server/src/main/kotlin/ru/fominmv/poms/server/api/ws/PathAwareWebSocketHandler.kt
+++
@@ -1,9 +0,0 @@
-package ru.fominmv.poms.server.api.ws
-
-import org.springframework.web.socket.WebSocketHandler
-
-import ru.fominmv.poms.server.api.PathAware
-
-interface PathAwareWebSocketHandler :
-    WebSocketHandler,
-    PathAware
