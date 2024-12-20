fab50cd0fce4ef0d141f33af2d8cb703565de4da
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-20 15:13:27 +0300

    Added SchedulerConfig class

---
+++ server/src/main/kotlin/ru/fominmv/poms/server/configs/SchedulerConfig.kt
@@ -0,0 +1,8 @@
+package ru.fominmv.poms.server.configs
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class SchedulerConfig
