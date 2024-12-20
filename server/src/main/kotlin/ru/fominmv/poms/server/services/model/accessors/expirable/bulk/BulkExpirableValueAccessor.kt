4f4c4bfa57f8864cf15b9e2b77c01c3a0a0e4434
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-20 19:47:44 +0300

    Tweaked BulkExpirableValueAccessor interface

--- server/src/main/kotlin/ru/fominmv/poms/server/services/model/accessors/expirable/bulk/BulkExpirableValueAccessor.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/services/model/accessors/expirable/bulk/BulkExpirableValueAccessor.kt
@@ -15,22 +15,28 @@ interface BulkExpirableValueAccessor<T : Expirable> : BulkValueAccessor<T> {
     override fun exists(): Boolean =
         exists(ExpirableState.ANY)
 
-    fun exists(state: ExpirableState, now: Instant = Instant.now()): Boolean
+    fun exists(state: ExpirableState, now: Instant = Instant.now()): Boolean =
+        count(state, now) > 0
 
     // Counting
 
     override fun count(): Long =
         count(ExpirableState.ANY)
 
-    fun count(state: ExpirableState, now: Instant = Instant.now()): Long
+    fun count(state: ExpirableState, now: Instant = Instant.now()): Long =
+        getAll(state, now).size.toLong()
 
     // Getting
+
+    // - All
 
     override fun getAll(): List<T> =
         getAll(ExpirableState.ANY)
 
-    fun getAll(state: ExpirableState, now: Instant = Instant.now()): List<T>
+    fun getAll(state: ExpirableState, now: Instant = Instant.now()): List<T> =
+        getAll(Pageable.unpaged(), state, now).content
 
+    // - Paged
 
     override fun getAll(pageable: Pageable): Page<T> =
         getAll(pageable, ExpirableState.ANY)
