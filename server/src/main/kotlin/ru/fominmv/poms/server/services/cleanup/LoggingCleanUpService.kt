6d16f4027f2d19bfd5aa45740c1b32d6f698b6f2
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2024-12-23 15:48:49 +0300

    Tweaked LoggingCleanUpService class

--- server/src/main/kotlin/ru/fominmv/poms/server/services/cleanup/LoggingCleanUpService.kt
+++ server/src/main/kotlin/ru/fominmv/poms/server/services/cleanup/LoggingCleanUpService.kt
@@ -2,22 +2,25 @@ package ru.fominmv.poms.server.services.cleanup
 
 import org.slf4j.LoggerFactory
 
+import ru.fominmv.poms.libs.commons.text.strings.ext.plural
 import ru.fominmv.poms.server.model.interfaces.immutable.Expirable
 import ru.fominmv.poms.server.services.model.accessors.expirable.ExpirableState
 import ru.fominmv.poms.server.services.model.accessors.expirable.bulk.BulkExpirableValueAccessor
 
 open class LoggingCleanUpService(
     private val accessor: BulkExpirableValueAccessor<*>,
-    private val targetTypeSingular: String,
-    private val targetTypePlural: String,
+    private val targetTypeSingular: String = DEFAULT_TARGET_TYPE_SINGULAR,
+    private val targetTypePlural: String = targetTypeSingular.plural(),
 ) : CleanUpService {
     companion object {
         inline operator fun <reified T : Expirable> invoke(
-            expiredBulkAccessor: BulkExpirableValueAccessor<*>,
-            targetTypeSingular: String = T::class.simpleName.orEmpty().ifBlank { "object" },
-            targetTypePlural: String = "${targetTypeSingular}s",
+            expiredBulkAccessor: BulkExpirableValueAccessor<T>,
+            targetTypeSingular: String = T::class.simpleName.orEmpty().ifBlank { DEFAULT_TARGET_TYPE_SINGULAR },
+            targetTypePlural: String = targetTypeSingular.plural(),
         ): LoggingCleanUpService =
             LoggingCleanUpService(expiredBulkAccessor, targetTypeSingular, targetTypePlural)
+
+        const val DEFAULT_TARGET_TYPE_SINGULAR = "object"
     }
 
     protected val logger = LoggerFactory.getLogger(javaClass)!!
